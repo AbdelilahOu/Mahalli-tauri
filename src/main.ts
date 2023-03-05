913d039c75b8dd1f727928308182316d54f82ff1
Abdelilah_OU <gxgamer710@gmail.com>
2023-03-05 12:50:33 +0000

    update imports main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createPinia } from "pinia";
-import { createApp, watch } from "vue";
 import i18n from "./plugins/i18n";
+import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
 import "./assets/main.css";
