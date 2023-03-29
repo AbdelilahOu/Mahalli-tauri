a0c9a791c38ebe13d9e71d46288ef1edebde09e8
Abdelilah_OU <gxgamer710@gmail.com>
2023-03-29 16:16:13 +0000

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { MotionPlugin, useMotion } from "@vueuse/motion";
+import { fade, slide } from "./utils/directivesEffect";
 import { createPinia } from "pinia";
 import i18n from "./plugins/i18n";
 import { createApp } from "vue";
@@ -7,43 +7,9 @@ import App from "./App.vue";
 import "./assets/main.css";
 
 createApp(App)
-  .directive("fade", {
-    mounted: (el: HTMLElement, bin) => {
-      useMotion(el, {
-        initial: {
-          opacity: 0,
-        },
-        enter: {
-          opacity: 1,
-          transition: {
-            delay: (bin.value + 1) * 100,
-          },
-        },
-      });
-    },
-  })
-  .directive("slide", {
-    mounted: (el: HTMLElement, bin) => {
-      useMotion(el, {
-        initial: {
-          opacity: 0,
-          x: 20,
-        },
-        enter: {
-          opacity: 1,
-          x: 0,
-          transition: {
-            delay: (bin.value + 1) * 100,
-          },
-        },
-      });
-    },
-    // beforeUpdate: () => "",
-  })
+  .directive("slide", { mounted: slide })
+  .directive("fade", { mounted: fade })
   .use(createPinia())
   .use(router)
   .use(i18n)
-  .use(MotionPlugin, {
-    directves: {},
-  })
   .mount("#app");
