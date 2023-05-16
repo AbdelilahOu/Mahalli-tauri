5121a1c76324de681dfa8c5b6617a4109ef81d78
Abdelilah_OU <gxgamer710@gmail.com>
2023-05-16 11:22:25 +0000

    update invoice table

--- src/components/InvoiceTable.tsx
+++ src/components/InvoiceTable.tsx
@@ -61,7 +61,7 @@ export const InvoiceTable = defineComponent({
             )
               .slice(pagination.value * 15, pagination.value * 15 + 15)
               .map((Invoice, index) => (
-                <tr v-slide={index}>
+                <tr v-fade={index} key={Invoice.id}>
                   <td class="p-2">
                     <span class="h-full w-full grid">
                       <UiCheckBox
