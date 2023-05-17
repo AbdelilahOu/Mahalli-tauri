cb451f0b83279dbf70defbe85b474a7aa1db0096
Abdelilah_OU <gxgamer710@gmail.com>
2023-05-17 10:23:04 +0000

    update stock store

--- src/stores/stockStore.ts
+++ src/stores/stockStore.ts
@@ -1,4 +1,4 @@
-import type { newStockMvmT, stockState, stockT } from "@/types";
+import type { newStockMvmT, stockMvmT, stockState } from "@/types";
 import database from "@/database/db";
 import { defineStore } from "pinia";
 
@@ -10,50 +10,49 @@ export const useStockStore = defineStore("StockStore", {
   },
   actions: {
     getAllStockMouvements: async function () {
-      const { db } = await database();
       try {
-        const stocks: stockT[] = await db.select(
+        const { db } = await database();
+        // get stocks
+        const stocks: stockMvmT[] = await db.select(
           "SELECT * FROM stock_mouvements ORDER BY id DESC"
         );
-        const invoiceItems: {
-          quantity: number;
-          stock_id: number;
-          invoice_id: number;
-        }[] = await db.select(
-          "SELECT quantity, invoice_id, stock_id  FROM invoice_items"
-        );
-        const commandItems: {
-          quantity: number;
-          stock_id: number;
-          command_id: number;
-        }[] = await db.select(
-          "SELECT quantity, command_id, stock_id  FROM command_items"
-        );
-        const products: { name: string; price: number; id: number }[] =
-          await db.select("SELECT name, price,id  FROM products");
-
-        this.stockMouvements = stocks.map((stock) => {
-          const product = products.find(
-            (product) => stock.product_id == product.id
+        // get joins
+        for await (const stock of stocks) {
+          const invoiceitem: any[] = await db.select(
+            "SELECT invoice_id FROM invoice_items WHERE stock_id = $1",
+            [stock.id]
           );
-          return {
-            ...stock,
-            product: { name: product?.name || "", price: product?.price || 0 },
-            commandItem: commandItems.find(
-              (item) => item.stock_id === stock.id
-            ),
-            invoiceItem: invoiceItems.find(
-              (item) => item.stock_id === stock.id
-            ),
+          const commanditem: any[] = await db.select(
+            "SELECT command_id FROM command_items WHERE stock_id = $1",
+            [stock.id]
+          );
+          const product: any[] = await db.select(
+            "SELECT name, price, id FROM products WHERE id = $1",
+            [stock.product_id]
+          );
+          // get index
+          const index = stocks.indexOf(stock);
+          // assign joins
+          stocks[index].commandItem = commanditem[0] as {
+            command_id: number;
           };
-        });
+          stocks[index].invoiceItem = invoiceitem[0] as {
+            invoice_id: number;
+          };
+          stocks[index].product = product[0] as {
+            name: string;
+            price: number;
+          };
+        }
+
+        this.stockMouvements = stocks;
       } catch (error) {
         console.log(error);
       }
     },
     createStockMouvement: async function (stockmvm: newStockMvmT) {
-      const { db } = await database();
       try {
+        const { db } = await database();
         await db.execute(
           "INSERT INTO stock_mouvements (model,product_id,quantity) VALUES ($1,$2,$3)",
           ["IN", stockmvm.productId, stockmvm.quantity]
