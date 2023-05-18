991860468de78c975eac759e0f461df6a68910bf
Abdelilah_OU <gxgamer710@gmail.com>
2023-05-18 09:14:32 +0000

    update invoice store

--- src/stores/invoiceStore.ts
+++ src/stores/invoiceStore.ts
@@ -9,6 +9,7 @@ import type {
 } from "@/types";
 import { defineStore } from "pinia";
 import database from "@/database/db";
+import { invoiceDetailsJoins, invoicesJoins } from "@/constants/dbQueryJson";
 
 export const useInvoiceStore = defineStore("InvoiceStore", {
   state: (): invoiceState => {
@@ -21,41 +22,9 @@ export const useInvoiceStore = defineStore("InvoiceStore", {
     getAllInvoices: async function () {
       try {
         const { db } = await database();
-        const invoices: invoiceT[] = await db.select(
-          "SELECT * FROM invoices ORDER BY id DESC"
-        );
-
-        for await (const invoice of invoices) {
-          const invoice_items: invoiceItemT[] = await db.select(
-            "SELECT * FROM invoice_items WHERE invoice_id = $1",
-            [invoice.id]
-          );
-          let client_name: { name: string }[] = await db.select(
-            "SELECT name FROM clients WHERE id = $1",
-            [invoice.client_id]
-          );
-          for await (const item of invoice_items) {
-            let product: { price: number; name: string }[] = await db.select(
-              "SELECT price ,name FROM products WHERE id = $1",
-              [item.product_id]
-            );
-            invoice_items[invoice_items.indexOf(item)]["product"] = product[0];
-          }
-          invoices[invoices.indexOf(invoice)]["invoiceItems"] =
-            invoice_items.map((invoice: invoiceItemT) => ({
-              ...invoice,
-              quantity: Math.abs(invoice.quantity),
-            }));
-          // map each invoice and create a total from quantity and price of its prodducts
-          invoices[invoices.indexOf(invoice)]["total"] = invoice_items.reduce(
-            (acc, curr) =>
-              (acc += Math.abs(curr.quantity) * curr.product.price),
-            0
-          );
-          invoices[invoices.indexOf(invoice)]["client_name"] =
-            client_name[0].name;
-        }
-        this.invoices = invoices;
+        const result = (await db.select(invoicesJoins)) as { data: string }[];
+        console.log(result);
+        this.invoices = result.map((c) => JSON.parse(c.data));
       } catch (error) {
         console.log(error);
       }
@@ -63,38 +32,8 @@ export const useInvoiceStore = defineStore("InvoiceStore", {
     getOneInvoice: async function (id: number) {
       try {
         const { db } = await database();
-        const invoice: invoiceT[] = await db.select(
-          "SELECT * FROM invoices WHERE id = $1",
-          [id]
-        );
-        const client: clientT[] = await db.select(
-          "SELECT * FROM clients WHERE id = $1",
-          [invoice[0].client_id]
-        );
-        let invoiceItems: invoiceDetailsItemT[] = await db.select(
-          "SELECT * FROM invoice_items WHERE invoice_id = $1",
-          [id]
-        );
-        for await (const item of invoiceItems) {
-          let product: {
-            name: string;
-            price: number;
-            tva: number;
-            description: string;
-          }[] = await db.select(
-            "SELECT name, price, tva, description FROM products WHERE id = $1",
-            [item.product_id]
-          );
-          invoiceItems[invoiceItems.indexOf(item)]["product"] = product[0];
-        }
-        this.invoice = {
-          ...invoice[0],
-          client: client[0],
-          invoiceItems: invoiceItems.map((item) => ({
-            ...item,
-            quantity: Math.abs(item.quantity),
-          })),
-        };
+        const result = (await db.select(invoiceDetailsJoins, [id])) as any[];
+        this.invoice = JSON.parse(result[0].data);
       } catch (error) {
         console.log(error);
       }
