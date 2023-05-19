e14b17513bcafe0d97297b09423ffdcb5ddde6f4
Abdelilah_OU <gxgamer710@gmail.com>
2023-05-19 11:27:35 +0000

    update stores

--- src/stores/stockStore.ts
+++ src/stores/stockStore.ts
@@ -1,6 +1,7 @@
 import type { newStockMvmT, stockMvmT, stockState } from "@/types";
 import database from "@/database/db";
 import { defineStore } from "pinia";
+import { stockJoins } from "@/constants/dbQueryJson";
 
 export const useStockStore = defineStore("StockStore", {
   state: (): stockState => {
@@ -12,40 +13,11 @@ export const useStockStore = defineStore("StockStore", {
     getAllStockMouvements: async function () {
       try {
         const { db } = await database();
-        // get stocks
-        const stocks: stockMvmT[] = await db.select(
-          "SELECT * FROM stock_mouvements ORDER BY id DESC"
-        );
-        // get joins
-        for await (const stock of stocks) {
-          const invoiceitem: any[] = await db.select(
-            "SELECT invoice_id FROM invoice_items WHERE stock_id = $1",
-            [stock.id]
-          );
-          const commanditem: any[] = await db.select(
-            "SELECT command_id FROM command_items WHERE stock_id = $1",
-            [stock.id]
-          );
-          const product: any[] = await db.select(
-            "SELECT name, price, id FROM products WHERE id = $1",
-            [stock.product_id]
-          );
-          // get index
-          const index = stocks.indexOf(stock);
-          // assign joins
-          stocks[index].commandItem = commanditem[0] as {
-            command_id: number;
-          };
-          stocks[index].invoiceItem = invoiceitem[0] as {
-            invoice_id: number;
-          };
-          stocks[index].product = product[0] as {
-            name: string;
-            price: number;
-          };
-        }
-
-        this.stockMouvements = stocks;
+        const res = (await db.select(stockJoins)) as {
+          data: string;
+        }[];
+        this.stockMouvements = res.map((r) => JSON.parse(r.data));
+        console.log(this.stockMouvements);
       } catch (error) {
         console.log(error);
       }
