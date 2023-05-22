c61c2236015a0be5fcb0886ecfb80598dcf3088e
AbdelilahOu <abdelilah4dev@gmail.com>
2023-05-22 11:57:19 +0000

    commands -> order

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import svgLoader from "vite-svg-loader";
-import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
