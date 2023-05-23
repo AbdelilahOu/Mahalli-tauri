ede63add96b33f67bfe5f5e3ab752d8ddd5a16c2
AbdelilahOu <abdelilah4dev@gmail.com>
2023-05-23 11:03:16 +0000

    fix click outside

--- src/components/ClientCreate.tsx
+++ src/components/ClientCreate.tsx
@@ -32,7 +32,7 @@ export const ClientCreate = defineComponent({
       }, 1000);
     };
     return () => (
-      <div class="w-1/2 h-fit z-50 gap-3 flex flex-col bg-white p-2 min-w-[350px]">
+      <div class="w-1/2 h-fit z-50 gap-3 rounded-md flex flex-col bg-white p-2 min-w-[350px]">
         <h1 class="font-semibold text-lg text-gray-800 border-b-2 border-b-gray-500 pb-2 uppercase text-center">
           {globalTranslate("Clients.create.title")}
         </h1>
