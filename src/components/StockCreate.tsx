a5a51b94bfa5bae3829035ba318d0bc4a47513db
AbdelilahOu <abdelilah4dev@gmail.com>
2023-05-24 10:39:03 +0000

    credi -> payment

--- src/components/StockCreate.tsx
+++ src/components/StockCreate.tsx
@@ -1,19 +1,18 @@
-import { defineComponent, reactive, ref, withModifiers } from "vue";
+import { globalTranslate } from "@/utils/globalTranslate";
 import { useProductStore } from "@/stores/productStore";
 import { useModalStore } from "@/stores/modalStore";
 import { useStockStore } from "@/stores/stockStore";
+import { defineComponent, reactive } from "vue";
 import { UiButton } from "./ui/UiButton";
 import { UiSelect } from "./ui/UiSelect";
 import { UiInput } from "./ui/UiInput";
 import { storeToRefs } from "pinia";
-import { globalTranslate } from "@/utils/globalTranslate";
 
 export const StockCreate = defineComponent({
   name: "StockCreate",
   components: { UiButton, UiInput, UiSelect },
   setup() {
     const { products } = storeToRefs(useProductStore());
-    const IsClicked = ref<boolean>(false);
     const stockMvm = reactive({
       productId: 0,
       quantity: 0,
@@ -27,13 +26,7 @@ export const StockCreate = defineComponent({
     };
     return () => (
       <div class="w-1/2 h-fit z-50 gap-3 flex flex-col bg-white p-2 min-w-[350px]">
-        <h1
-          onClick={withModifiers(
-            () => (IsClicked.value = !IsClicked.value),
-            ["self"]
-          )}
-          class="font-semibold text-lg text-gray-800 border-b-2 border-b-gray-500 pb-2 uppercase text-center"
-        >
+        <h1 class="font-semibold text-lg text-gray-800 border-b-2 border-b-gray-500 pb-2 uppercase text-center">
           {globalTranslate("Stocks.create.title")}
         </h1>
         <div class="h-full w-full flex flex-col gap-2">
@@ -43,7 +36,6 @@ export const StockCreate = defineComponent({
               id: product.id,
             }))}
             onSelect={(id: number) => (stockMvm.productId = id)}
-            IsClickedOuside={IsClicked.value}
           >
             {globalTranslate("Stocks.create.select")}
           </UiSelect>
