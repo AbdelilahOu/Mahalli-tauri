25b6e9e66e1b8e63bf1e2a9fa7c7b4097e67ba28
AbdelilahOu <abdelilah4dev@gmail.com>
2023-05-24 10:38:05 +0000

    credi -> payment

--- src/components/ProductCreate.tsx
+++ src/components/ProductCreate.tsx
@@ -5,13 +5,15 @@ import { UiButton } from "./ui/UiButton";
 import { UiInput } from "./ui/UiInput";
 import { useModalStore } from "@/stores/modalStore";
 import { globalTranslate } from "@/utils/globalTranslate";
+import { UiUploader } from "./ui/UiUploader";
+import { ImagesFiles } from "@/constants/FileTypes";
 
 export const ProductCreate = defineComponent({
   name: "ProductCreate",
   components: { UiButton, UiInput },
   setup() {
     const isFlash = ref<boolean>(false);
-    const newProduct = reactive<newProductT>({
+    const Product = reactive<newProductT>({
       name: "",
       price: 0,
       quantity: 0,
@@ -20,8 +22,8 @@ export const ProductCreate = defineComponent({
     });
     const createNewProduct = () => {
       isFlash.value = true;
-      if (newProduct.name !== "") {
-        useProductStore().createOneProduct(newProduct);
+      if (Product.name !== "") {
+        useProductStore().createOneProduct(Product);
         useModalStore().updateModal({ key: "show", value: false });
       }
       setTimeout(() => {
@@ -29,42 +31,49 @@ export const ProductCreate = defineComponent({
       }, 1000);
     };
     return () => (
-      <div class="w-1/2 h-fit z-50 gap-3 flex flex-col bg-white p-2 min-w-[350px]">
+      <div class="w-1/2 h-fit rounded-md z-50 gap-3 flex flex-col bg-white p-2 min-w-[350px]">
         <h1 class="font-semibold text-lg text-gray-800 border-b-2 border-b-gray-500 pb-2 uppercase text-center">
           {globalTranslate("Products.create.title")}
         </h1>
         <div class="h-full w-full flex flex-col gap-2">
+          <div class="w-full h-fit flex justify-center">
+            <UiUploader
+              name="Image"
+              extensions={ImagesFiles}
+              onSave={(image) => (Product.image = image)}
+            />
+          </div>
           <UiInput
-            IsEmpty={isFlash.value && newProduct["name"] == ""}
+            IsEmpty={isFlash.value && Product["name"] == ""}
             OnInputChange={(value) =>
-              (newProduct["name"] =
+              (Product["name"] =
                 typeof value == "string" ? value : JSON.stringify(value))
             }
             Type="text"
             PlaceHolder={globalTranslate("Products.create.placeholders[0]")}
           />
           <UiInput
-            IsEmpty={isFlash.value && newProduct["price"] == 0}
-            OnInputChange={(value) => (newProduct["price"] = Number(value))}
+            IsEmpty={isFlash.value && Product["price"] == 0}
+            OnInputChange={(value) => (Product["price"] = Number(value))}
             Type="Number"
             PlaceHolder={globalTranslate("Products.create.placeholders[2]")}
           />
           <UiInput
-            IsEmpty={isFlash.value && newProduct["tva"] == 0}
-            OnInputChange={(value) => (newProduct["tva"] = Number(value))}
+            IsEmpty={isFlash.value && Product["tva"] == 0}
+            OnInputChange={(value) => (Product["tva"] = Number(value))}
             Type="Number"
             PlaceHolder={globalTranslate("Products.create.placeholders[3]")}
           />
           <UiInput
-            IsEmpty={isFlash.value && newProduct["quantity"] == 0}
-            OnInputChange={(value) => (newProduct["quantity"] = Number(value))}
+            IsEmpty={isFlash.value && Product["quantity"] == 0}
+            OnInputChange={(value) => (Product["quantity"] = Number(value))}
             Type="Number"
             PlaceHolder={globalTranslate("Products.create.placeholders[4]")}
           />
           <UiInput
-            IsEmpty={isFlash.value && newProduct["description"] == ""}
+            IsEmpty={isFlash.value && Product["description"] == ""}
             OnInputChange={(value) =>
-              (newProduct["description"] =
+              (Product["description"] =
                 typeof value == "string" ? value : JSON.stringify(value))
             }
             Type="textarea"
