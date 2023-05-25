dec68aef3d5b4422b7d063d23a0cc8c540592822
Abdelilah_OU <gxgamer710@gmail.com>
2023-05-25 09:30:20 +0000

    credi -> payment

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,6 +1,8 @@
 import { BaseDirectory, createDir, copyFile, exists } from "@tauri-apps/api/fs";
 import { appDataDir, sep, join } from "@tauri-apps/api/path";
 
+// C:\Users\abdel\AppData\Roaming\whatisthis
+
 export const updateFile = async (path: string, name: string) => {
   if (path.split(sep).length > 1) {
     return await saveFile(path, name);
@@ -9,18 +11,22 @@ export const updateFile = async (path: string, name: string) => {
 };
 
 export const saveFile = async (path: string, name: string) => {
+  if (!path) return "";
   const RightFolder = name === "Image" ? "Images" : "Docs";
   try {
     const fileName = path.split(sep)[path.split(sep).length - 1];
     await createFolder(RightFolder);
+
     const filePath = await join(await appDataDir(), RightFolder, fileName);
+
     await copyFile(path, filePath, {
       dir: BaseDirectory.AppData,
     });
 
-    return fileName;
+    return filePath;
   } catch (error) {
     console.log("sth went wrong", error);
+    return "";
   }
 };
 
