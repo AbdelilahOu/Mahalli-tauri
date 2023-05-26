f0b720068306bdc2668e117bc9a9738f11c8c417
AbdelilahOu <abdelilah4dev@gmail.com>
2023-05-26 12:14:03 +0000

    inject db instance to pinia

---
+++ src/views/PaymentView.tsx
@@ -0,0 +1,82 @@
+import { defineComponent, onBeforeMount, ref, Transition } from "vue";
+import { globalTranslate } from "@/utils/globalTranslate";
+import { PaymentTable } from "@/components/PaymentTable";
+import { UiButton } from "@/components/ui/UiButton";
+import { useModalStore } from "@/stores/modalStore";
+import { UiInput } from "@/components/ui/UiInput";
+import UiIcon from "@/components/ui/UiIcon.vue";
+import { storeToRefs } from "pinia";
+import { usePaymentStore } from "@/stores/paymentStore";
+
+export const PaymentView = defineComponent({
+  name: "Payments",
+  components: { PaymentTable, UiButton, UiInput, UiIcon },
+  setup() {
+    const modalStore = useModalStore();
+    const PaymentStore = usePaymentStore();
+    const { Payments } = storeToRefs(PaymentStore);
+    //
+    const searchQuery = ref<string>("");
+
+    //
+    onBeforeMount(() => {
+      if (!Payments.value.length) PaymentStore.getAllPayments();
+    });
+    //
+    const updateModal = (name: string) => {
+      modalStore.updateModal({ key: "show", value: true });
+      modalStore.updateModal({ key: "name", value: name });
+    };
+    //
+    const sortPaymentsBy = (by: string) => {};
+
+    return () => (
+      <main class="w-full h-full px-3">
+        <div class="w-full h-full flex flex-col items-start justify-start">
+          <Transition appear>
+            <div class="flex justify-between w-full gap-9 my-1">
+              <div class="w-1/3">
+                <UiInput
+                  IsEmpty={false}
+                  OnInputChange={(value) =>
+                    (searchQuery.value =
+                      typeof value !== "string"
+                        ? JSON.stringify(value).toLocaleLowerCase()
+                        : value.toLocaleLowerCase())
+                  }
+                  Type="text"
+                  PlaceHolder={globalTranslate("Global.search")}
+                >
+                  <UiIcon
+                    class=" fill-gray-400 cursor-default hover:bg-white"
+                    name="search"
+                  />
+                </UiInput>
+              </div>
+              <div class="w-1/4 flex gap-2">
+                <UiButton
+                  colorTheme="a"
+                  Click={() => updateModal("PaymentCreate")}
+                >
+                  <UiIcon
+                    class=" fill-gray-900 cursor-default hover:bg-transparent"
+                    name="add"
+                  />{" "}
+                  {globalTranslate("Payments.index.addButton")}
+                </UiButton>
+              </div>
+            </div>
+          </Transition>
+
+          <Transition appear>
+            <PaymentTable
+              FilterParam={searchQuery.value}
+              sortBy={(by: string) => sortPaymentsBy(by)}
+              Payment={Payments.value}
+            />
+          </Transition>
+        </div>
+      </main>
+    );
+  },
+});
