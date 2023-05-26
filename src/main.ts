fec8f50fe012427a0d4e308327f47c0f957a68d8
Abdelilah_OU <gxgamer710@gmail.com>
2023-05-26 12:19:32 +0000

    inject db instance to pinia

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,64 @@
-import { fade, slide } from "./utils/directivesEffect";
 import messages from "@intlify/unplugin-vue-i18n/messages";
+import { createApp, type DirectiveBinding } from "vue";
+import type Database from "tauri-plugin-sql-api";
+import database from "tauri-plugin-sql-api";
+import { useMotion } from "@vueuse/motion";
 import { createI18n } from "vue-i18n";
-import { createApp } from "vue";
+import { createPinia } from "pinia";
 import router from "./router";
 import App from "./App.vue";
-import pinia from "./pinia";
 import "./assets/main.css";
-import database from "tauri-plugin-sql-api";
-import type Database from "tauri-plugin-sql-api";
 
 const locale = localStorage.getItem("locale");
 
 (async () => {
   const db = await database.load("sqlite:db.sqlite");
+
+  const pinia = createPinia();
+
+  pinia.use(({ store }) => {
+    store.db = db;
+  });
+
   createApp(App)
-    .directive("slide", { mounted: slide })
-    .directive("fade", { mounted: fade })
+    .directive("slide", {
+      mounted: (el: HTMLElement, bin: DirectiveBinding) => {
+        useMotion(el, {
+          initial: {
+            opacity: 0,
+            x: 20,
+          },
+          enter: {
+            opacity: 1,
+            x: 0,
+            transition: {
+              delay: (bin.value + 1) * 100,
+            },
+          },
+        });
+      },
+    })
+    .directive("fade", {
+      mounted: (el: HTMLElement, bin: DirectiveBinding) => {
+        useMotion(el, {
+          initial: {
+            opacity: 0,
+          },
+          enter: {
+            opacity: 1,
+            transition: {
+              delay: (bin.value + 1) * 100,
+            },
+          },
+        });
+      },
+    })
     .directive("drop", {
       mounted: (el: HTMLElement, bin) => {
         el.addEventListener("dragstart", (e: DragEventInit) => {});
       },
     })
-    .use(
-      pinia.use(({ store }) => {
-        store.db = db;
-      })
-    )
+    .use(pinia)
     .use(router)
     .use(
       createI18n({
