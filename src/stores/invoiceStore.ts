c7b7f46d45bc2fe8be8f7abde9e1647aa2b27880
AbdelilahOu <abdelilah4dev@gmail.com>
2023-05-26 12:11:52 +0000

    inject db instance to pinia

--- src/stores/invoiceStore.ts
+++ src/stores/invoiceStore.ts
@@ -1,6 +1,5 @@
 import { invoiceDetailsJoins, invoicesJoins } from "@/database/dbQueryJson";
 import type { invoiceState, updateInvoiceT, newInvoiceT } from "@/types";
-import database from "@/database/db";
 import { defineStore } from "pinia";
 
 export const useInvoiceStore = defineStore("InvoiceStore", {
@@ -13,8 +12,7 @@ export const useInvoiceStore = defineStore("InvoiceStore", {
   actions: {
     getAllInvoices: async function () {
       try {
-        const { db } = await database();
-        const result: { data: string }[] = await db.select(invoicesJoins);
+        const result: { data: string }[] = await this.db.select(invoicesJoins);
         this.invoices = result.map((c) => JSON.parse(c.data));
       } catch (error) {
         console.log(error);
@@ -22,8 +20,7 @@ export const useInvoiceStore = defineStore("InvoiceStore", {
     },
     getOneInvoice: async function (id: number) {
       try {
-        const { db } = await database();
-        const result: any[] = await db.select(invoiceDetailsJoins, [id]);
+        const result: any[] = await this.db.select(invoiceDetailsJoins, [id]);
         this.invoice = JSON.parse(result[0].data);
       } catch (error) {
         console.log(error);
@@ -31,26 +28,25 @@ export const useInvoiceStore = defineStore("InvoiceStore", {
     },
     createOneInvoice: async function (invoice: newInvoiceT) {
       try {
-        const { db } = await database();
         const { client_id, invoiceItems, status } = invoice;
 
-        await db.execute(
+        await this.db.execute(
           "INSERT INTO invoices (client_id,total,status) VALUES ($1,$2,$3)",
           [client_id, 0, status]
         );
-        const id: { id: number }[] = await db.select(
+        const id: { id: number }[] = await this.db.select(
           "SELECT max(id) as id FROM invoices"
         );
         for await (const { quantity, product_id } of invoiceItems) {
-          await db.execute(
+          await this.db.execute(
             "INSERT INTO stock_mouvements (quantity,model,product_id) VALUES ($1,$2,$3)",
             [-quantity, "OUT", product_id]
           );
-          const stock_id: { id: number }[] = await db.select(
+          const stock_id: { id: number }[] = await this.db.select(
             "SELECT max(id) as id FROM stock_mouvements"
           );
 
-          await db.execute(
+          await this.db.execute(
             "INSERT INTO invoice_items (quantity,product_id,invoice_id,stock_id) VALUES ($1,$2,$3,$4)",
             [quantity, product_id, id[0].id, stock_id[0].id]
           );
@@ -62,34 +58,33 @@ export const useInvoiceStore = defineStore("InvoiceStore", {
     },
     updateOneInvoice: async function (id: number, invoice: updateInvoiceT) {
       try {
-        const { db } = await database();
         const { total, client_id, invoiceItems } = invoice;
 
-        await db.execute(
+        await this.db.execute(
           "UPDATE invoices SET total = $1 , client_id = $2 WHERE id = $3",
           [total, client_id, id]
         );
         for await (const item of invoiceItems) {
           if (item.id) {
-            await db.execute(
+            await this.db.execute(
               "UPDATE invoice_items SET product_id = $1 , quantity = $2 WHERE id = $3",
               [item.product_id, -Number(item.quantity), item.id]
             );
 
-            await db.execute(
+            await this.db.execute(
               "UPDATE stock_mouvements SET quantity = $1 WHERE id = $2",
               [-Number(item.quantity), item.stock_id]
             );
           } else {
-            await db.execute(
+            await this.db.execute(
               "INSERT INTO stock_mouvements (quantity,model,product_id) VALUES ($1,$2,$3)",
               [-Number(item.quantity), "OUT", item.product_id]
             );
-            const stock_id: { id: number }[] = await db.select(
+            const stock_id: { id: number }[] = await this.db.select(
               "SELECT max(id) as id FROM stock_mouvements"
             );
 
-            await db.execute(
+            await this.db.execute(
               "INSERT INTO invoice_items (quantity,product_id,invoice_id,stock_id) VALUES ($1,$2,$3,$4)",
               [-Number(item.quantity), item.product_id, id, stock_id[0].id]
             );
@@ -102,9 +97,7 @@ export const useInvoiceStore = defineStore("InvoiceStore", {
     },
     deleteOneInvoice: async function (id: number) {
       try {
-        const { db } = await database();
-
-        await db.execute("DELETE FROM invoices WHERE id = $1", [id]);
+        await this.db.execute("DELETE FROM invoices WHERE id = $1", [id]);
         this.getAllInvoices();
       } catch (error) {
         console.log(error);
@@ -112,9 +105,7 @@ export const useInvoiceStore = defineStore("InvoiceStore", {
     },
     deleteOneinvoiceItem: async function (id: number) {
       try {
-        const { db } = await database();
-
-        await db.execute("DELETE FROM invoice_items WHERE id = $1", [id]);
+        await this.db.execute("DELETE FROM invoice_items WHERE id = $1", [id]);
         this.getAllInvoices();
       } catch (error) {
         console.log(error);
