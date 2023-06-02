9c3d6948737dff0d30822afda14d1d7051e21112
Abdelilah_OU <gxgamer710@gmail.com>
2023-06-02 11:18:02 +0000

    update charts

--- src/views/StocksView.tsx
+++ src/views/StocksView.tsx
@@ -32,54 +32,52 @@ export const StocksView = defineComponent({
       modalStore.updateModal({ key: "show", value: true });
       modalStore.updateModal({ key: "name", value: name });
     };
-    return () => {
-      return (
-        <main class="w-full h-full px-3">
-          <div class="w-full h-full flex flex-col items-start justify-start">
-            <Transition appear>
-              <div class="flex justify-between w-full gap-9 my-1">
-                <div class="w-1/3">
-                  <UiInput
-                    IsEmpty={false}
-                    OnInputChange={(value) =>
-                      (searchQuery.value =
-                        typeof value !== "string"
-                          ? JSON.stringify(value)
-                          : value.toLocaleLowerCase())
-                    }
-                    Type="text"
-                    PlaceHolder={globalTranslate("Global.search")}
-                  >
-                    <UiIcon
-                      class=" fill-gray-400 cursor-default hover:bg-white"
-                      name="search"
-                    />
-                  </UiInput>
-                </div>
-                <div class="w-1/4 flex gap-2">
-                  <UiButton
-                    colorTheme="a"
-                    Click={() => updateModal("StockCreate")}
-                  >
-                    <UiIcon
-                      class=" fill-gray-900 cursor-default hover:bg-transparent"
-                      name="add"
-                    />{" "}
-                    {globalTranslate("Stocks.index.addButton")}
-                  </UiButton>
-                </div>
+    return () => (
+      <main class="w-full h-full px-3">
+        <div class="w-full h-full flex flex-col items-start justify-start">
+          <Transition appear>
+            <div class="flex justify-between w-full gap-9 my-1">
+              <div class="w-1/3">
+                <UiInput
+                  IsEmpty={false}
+                  OnInputChange={(value) =>
+                    (searchQuery.value =
+                      typeof value !== "string"
+                        ? JSON.stringify(value)
+                        : value.toLocaleLowerCase())
+                  }
+                  Type="text"
+                  PlaceHolder={globalTranslate("Global.search")}
+                >
+                  <UiIcon
+                    class=" fill-gray-400 cursor-default hover:bg-white"
+                    name="search"
+                  />
+                </UiInput>
               </div>
-            </Transition>
+              <div class="w-1/4 flex gap-2">
+                <UiButton
+                  colorTheme="a"
+                  Click={() => updateModal("StockCreate")}
+                >
+                  <UiIcon
+                    class=" fill-gray-900 cursor-default hover:bg-transparent"
+                    name="add"
+                  />{" "}
+                  {globalTranslate("Stocks.index.addButton")}
+                </UiButton>
+              </div>
+            </div>
+          </Transition>
 
-            <Transition appear>
-              <StockTable
-                FilterParam={searchQuery.value}
-                Stock={stockMouvements.value}
-              />
-            </Transition>
-          </div>
-        </main>
-      );
-    };
+          <Transition appear>
+            <StockTable
+              FilterParam={searchQuery.value}
+              Stock={stockMouvements.value}
+            />
+          </Transition>
+        </div>
+      </main>
+    );
   },
 });
