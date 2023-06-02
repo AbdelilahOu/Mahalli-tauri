9debb8c3d3fa677107e5e87c6849fc179218c608
Abdelilah_OU <gxgamer710@gmail.com>
2023-06-02 11:16:25 +0000

    update charts

---
+++ src/constants/chartOptions.ts
@@ -0,0 +1,32 @@
+export const chartOptions = {
+  responsive: true,
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+      ticks: {
+        color: "rgba(25,23,17,0.6)",
+        textStrokeWidth: 10,
+      },
+      border: {
+        display: false,
+      },
+    },
+    y: {
+      grid: {
+        lineWidth: 1,
+        drawBorder: false,
+      },
+      border: {
+        display: false,
+      },
+      ticks: {
+        color: "rgba(25,23,17,0.6)",
+        min: 0,
+        textStrokeWidth: 1,
+        padding: 10,
+      },
+    },
+  },
+};
