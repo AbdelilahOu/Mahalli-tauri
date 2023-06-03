2fc140c8b9aa1daf593c7c145d73d04b921bf5a3
AbdelilahOu <abdelilah4dev@gmail.com>
2023-06-03 18:48:13 +0000

    using lodash

--- src/components/ChartHolder.tsx
+++ src/components/ChartHolder.tsx
@@ -7,9 +7,19 @@ export const ChartHolder = defineComponent({
   },
   setup({ title }, { slots }) {
     return () => (
-      <div class="w-full h-full flex flex-col">
-        <div class="w-full h-full flex">{slots.default && slots.default()}</div>
-        <h1>{title}</h1>
+      <div
+        class={`${
+          title ? "grid-rows-[50px_1fr]" : "grid-rows-1"
+        } w-full h-full grid overflow-hidden gap-1 grid-cols-1  rounded-md shadow-md`}
+      >
+        {title && (
+          <div class="w-full h-full flex items-center bg-slate-100">
+            <h1 class="m-2 w-full text-center">{title}</h1>
+          </div>
+        )}
+        <div class="w-full h-full p-2 flex">
+          {slots.default && slots.default()}
+        </div>
       </div>
     );
   },
