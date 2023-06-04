a4b03ed1b700e605b39e621915b365c29f2d19c7
Abdelilah_OU <gxgamer710@gmail.com>
2023-06-04 08:33:36 +0000

    using lodash

--- src/components/ChartHolder.tsx
+++ src/components/ChartHolder.tsx
@@ -2,19 +2,16 @@ import { defineComponent } from "vue";
 
 export const ChartHolder = defineComponent({
   name: "ChartHolder",
-  props: {
-    title: String,
-  },
-  setup({ title }, { slots }) {
+  setup({}, { slots }) {
     return () => (
       <div
         class={`${
-          title ? "grid-rows-[50px_1fr]" : "grid-rows-1"
-        } w-full h-full grid overflow-hidden gap-1 grid-cols-1  rounded-md shadow-md`}
+          slots.title ? "grid-rows-[50px_1fr]" : "grid-rows-1"
+        } w-full h-fit grid overflow-hidden gap-1 grid-cols-1  rounded-md shadow-md`}
       >
-        {title && (
+        {slots.title && (
           <div class="w-full h-full flex items-center bg-slate-100">
-            <h1 class="m-2 w-full text-center">{title}</h1>
+            {slots.title()}
           </div>
         )}
         <div class="w-full h-full p-2 flex">
