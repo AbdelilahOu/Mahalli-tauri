333617e2dea376d23c06e2493c052db110c12100
AbdelilahOu <abdelilah4dev@gmail.com>
2023-06-04 15:08:41 +0000

    using lodash

--- src/constants/chartOptions.ts
+++ src/constants/chartOptions.ts
@@ -30,3 +30,37 @@ export const chartOptions = {
     },
   },
 };
+
+export const optionsWoTicks = {
+  responsive: true,
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+      ticks: {
+        color: "rgba(25,23,17,0.6)",
+        textStrokeWidth: 10,
+      },
+      border: {
+        display: false,
+      },
+    },
+    y: {
+      grid: {
+        lineWidth: 0.5,
+        drawBorder: false,
+      },
+      border: {
+        display: false,
+      },
+      ticks: {
+        display: false,
+        // color: "rgba(25,23,17,0.6)",
+        // min: 0,
+        // textStrokeWidth: 1,
+        // padding: 10,
+      },
+    },
+  },
+};
