0b518d48d83c4f6a4d5bf187b03cec85d4034ba3
Abdelilah_OU <gxgamer710@gmail.com>
2023-06-18 14:41:01 +0000

    rename stores

--- src/utils/Oauth.ts
+++ src/utils/Oauth.ts
@@ -64,18 +64,13 @@ export const signOut = () => {
 };
 
 export const login = () => {
-  // Wait for callback from tauri oauth plugin
   listen("oauth://url", (data) => {
     console.log("listened", data);
     googleSignIn(data.payload as string);
   });
 
-  // Start tauri oauth plugin. When receive first request
-  // When it starts, will return the server port
-  // it will kill the server
   invoke("plugin:oauth|start", {
     config: {
-      // Optional config, but use here to more friendly callback page
       response: callbackTemplate,
     },
   }).then((port: any) => {
