cc290540f730a2b61a5a59b5eccc6b9c2778fc6c
Abdelilah_OU <gxgamer710@gmail.com>
2023-06-21 08:43:14 +0000

    update ui

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -34,16 +34,16 @@
 //     }
 // }
 
-// #[tauri::command]
-// async fn command_name(csv_path: String) -> Result<Vec<Record>, csv::Error> {
-//     let records = get_csv_data(csv_path).await;
-//     return records;
-// }
+#[tauri::command]
+fn command_name(csv_path: String) -> Vec<Record> {
+    let records = get_csv_data(csv_path);
+    return records;
+}
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_oauth::init())
-        // .invoke_handler(tauri::generate_handler![command_name])
+        .invoke_handler(tauri::generate_handler![command_name])
         .plugin(tauri_plugin_sql::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
