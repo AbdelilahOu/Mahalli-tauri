ed8d6f153cad2d363418272140951e4c2cb2479c
AbdelilahOu <abdelilah4dev@gmail.com>
2023-07-09 17:09:29 +0000

    Work on csv

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,55 +4,7 @@
 )]
 // modes
 mod csv_thingy;
-use crate::csv_thingy::schema::ProductRecord;
-
-use tauri::api::process::Command;
-use tauri::api::process::CommandEvent;
-
-#[tauri::command]
-fn get_csv_records(csv_path: String) -> Result<Vec<ProductRecord>, String> {
-    let mut records_array = Vec::<ProductRecord>::new();
-    let reader = csv::ReaderBuilder::new().from_path(csv_path);
-
-    match reader {
-        Ok(mut r) => {
-            for record in r.deserialize() {
-                match record {
-                    Ok(r) => {
-                        records_array.push(r);
-                    }
-                    Err(e) => {
-                        println!(
-                            "{:?},{:?}",
-                            String::from("Theres an error with this record"),
-                            e
-                        );
-                    }
-                }
-            }
-            Ok(records_array)
-        }
-        Err(e) => {
-            println!("{:?}", e);
-            return Err(String::from("hi theres an error"));
-        }
-    }
-}
-
-#[tauri::command]
-async fn export_db_csv() {
-    let (mut rx, mut child) = Command::new("sqlite3")
-        .args(["serve"])
-        .spawn()
-        .expect("Failed to spawn packaged node");
-
-    while let Some(event) = rx.recv().await {
-        if let CommandEvent::Stdout(line) = event {
-            println!("{:?}", line);
-            child.write("message from Rust\n".as_bytes()).unwrap();
-        }
-    }
-}
+use crate::csv_thingy::exp_emp::*;
 
 fn main() {
     tauri::Builder::default()
