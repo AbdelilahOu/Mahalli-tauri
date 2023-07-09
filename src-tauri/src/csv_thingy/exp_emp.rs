af721d42ff113c557b905e31211c7e9115931286
Abdelilah_OU <gxgamer710@gmail.com>
2023-07-09 17:18:47 +0000

    Work on csv

--- src-tauri/src/csv_thingy/exp_emp.rs
+++ src-tauri/src/csv_thingy/exp_emp.rs
@@ -8,52 +8,45 @@ use super::schema::{
     SellerRecord, StockMouvementRecord,
 };
 
-#[tauri::command]
-pub async fn export_db_csv() {
-    let (mut rx, mut child) = Command::new("sqlite3")
-        .args(["serve"])
-        .spawn()
-        .expect("Failed to spawn packaged node");
-
-    while let Some(event) = rx.recv().await {
-        if let CommandEvent::Stdout(line) = event {
-            println!("{:?}", line);
-            child.write("message from Rust\n".as_bytes()).unwrap();
-        }
-    }
-}
-
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub enum TableRecord {
     Client(Vec<ClientRecord>),
     Product(Vec<ProductRecord>),
+    Invoice(Vec<InvoiceRecord>),
 }
 
 #[tauri::command]
 pub fn get_csv_records(csv_path: String, table: Option<String>) -> Result<TableRecord, String> {
     match table {
         Some(a) => {
-            if a == String::from("clients") {
-                match read_csv::<ClientRecord>(csv_path) {
-                    Ok(r) => Ok(TableRecord::Client(r)),
+            let records = match a.as_str() {
+                "clients" => match read_csv::<ClientRecord>(csv_path) {
+                    Ok(r) => TableRecord::Client(r),
                     Err(e) => {
                         println!("{:?}", e);
-                        Err(e)
+                        return Err(e);
                     }
-                }
-            } else if a == String::from("products") {
-                match read_csv::<ProductRecord>(csv_path) {
-                    Ok(r) => Ok(TableRecord::Product(r)),
+                },
+                "products" => match read_csv::<ProductRecord>(csv_path) {
+                    Ok(r) => TableRecord::Product(r),
                     Err(e) => {
                         println!("{:?}", e);
-                        Err(e)
+                        return Err(e);
                     }
-                }
-            } else {
-                Err(String::from("this table doesnt exist"))
-            }
+                },
+                "invoices" => match read_csv::<InvoiceRecord>(csv_path) {
+                    Ok(r) => TableRecord::Invoice(r),
+                    Err(e) => {
+                        println!("{:?}", e);
+                        return Err(e);
+                    }
+                },
+                // Handle other table names and their corresponding record types
+                _ => return Err(String::from("This table doesn't exist")),
+            };
+            Ok(records)
         }
-        None => Err(String::from("didn't specify the table")),
+        None => Err(String::from("Didn't specify the table")),
     }
 }
 
@@ -85,6 +78,21 @@ fn read_csv<T: for<'de> serde::Deserialize<'de> + Debug>(
         Err(e) => {
             println!("{:?}", e);
             Err(String::from("hi theres an error"))
+        }
+    }
+}
+
+#[tauri::command]
+pub async fn export_db_csv() {
+    let (mut rx, mut child) = Command::new("sqlite3")
+        .args(["serve"])
+        .spawn()
+        .expect("Failed to spawn packaged node");
+
+    while let Some(event) = rx.recv().await {
+        if let CommandEvent::Stdout(line) = event {
+            println!("{:?}", line);
+            child.write("message from Rust\n".as_bytes()).unwrap();
         }
     }
 }
