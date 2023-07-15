0b3296e5062895b57cd476d58001be24bac5710b
Abdelilah_OU <gxgamer710@gmail.com>
2023-07-15 13:12:13 +0000

    csv-feature-done

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -22,9 +22,9 @@ export const saveFile = async (path: string, name: string) => {
   try {
     const fileName = path.split(sep)[path.split(sep).length - 1];
     await createFolder(RightFolder);
-
+    //
     const filePath = await join(await appDataDir(), RightFolder, fileName);
-
+    //
     await copyFile(path, filePath, {
       dir: BaseDirectory.AppData,
     });
