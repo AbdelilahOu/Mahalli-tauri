ff9d7ba608c96525f619752a9fc1bc0a669ff80d
AbdelilahOu <abdelilah4dev@gmail.com>
2023-07-18 10:42:47 +0000

    FIREBASE

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,11 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB9qbZa0tSzwDxjRcfC2wroGnTSf7j9vkQ",
   authDomain: "stock-management-a3276.firebaseapp.com",
