d4b50c8210f8a90ce7fd83bcf0c9ae640fd640fc
Abdelilah_OU <gxgamer710@gmail.com>
2023-07-18 10:43:57 +0000

    FORMAT DATE UPDATE

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,9 +1,8 @@
-export const formatDate = (theDate: string): string => {
-  return new Date(theDate).toLocaleDateString("fr-fr", {
+export const formatDate = (theDate: string): string =>
+  new Date(theDate).toLocaleDateString("fr-fr", {
     day: "numeric",
     month: "numeric",
     year: "numeric",
     hour: "2-digit",
     minute: "2-digit",
   });
-};
