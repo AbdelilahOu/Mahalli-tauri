8b88467c98d8f788e8423ece9a968f5ec3aa3563
AbdelilahOu <abdelilah4dev@gmail.com>
2023-07-24 16:04:39 +0000

    migrating backend to rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,15 +2,25 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+
+#[macro_use]
+extern crate diesel;
+extern crate diesel_migrations;
+extern crate dotenv;
+
 // modes
 mod csvparsing;
+mod db;
+mod models;
+mod queries;
+mod schema;
+
 use crate::csvparsing::{export::*, import::*};
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![get_csv_records, export_db_csv])
         .plugin(tauri_plugin_oauth::init())
-        .plugin(tauri_plugin_sql::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
