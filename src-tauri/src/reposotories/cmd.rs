e4cc47f5c80bf52cddb80c946bdb093675ee4b9b
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-07-24 16:56:46 +0000

    work on products

--- src-tauri/src/reposotories/cmd.rs
+++ src-tauri/src/reposotories/cmd.rs
@@ -12,5 +12,20 @@ pub fn get_products() -> Vec<Product> {
 #[tauri::command]
 pub fn get_product(id: i32) -> Product {
     let result = product_repo::get_product(id);
-    return result;
+    result
 }
+
+#[tauri::command]
+pub fn delete_product(id: i32) -> usize {
+    let result = product_repo::delete_product(id);
+    result
+}
+
+#[tauri::command]
+pub fn create_product(new_product: NewProduct) -> usize {
+    let result = product_repo::create_product(new_product);
+    result
+}
+
+#[tauri::command]
+pub fn update_product() {}
