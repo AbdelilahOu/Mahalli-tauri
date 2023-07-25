9519f1697a88a436eb00c1fe2b956b83e3d3c737
AbdelilahOu <abdelilah4dev@gmail.com>
2023-07-25 14:24:56 +0000

    delete sql plugin

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -7,25 +7,19 @@ use std::path;
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
-
     let _env = env::var("DEV_ENV");
-
     match _env {
         Ok(_env) => {
             let database_url = &env::var("DATABASE_URL").unwrap();
-
             SqliteConnection::establish(&database_url)
                 .expect(&format!("Error connecting to {}", &database_url))
         }
         Err(_) => {
             println!("Not dev");
-
             let database_url = path::Path::new(&tauri::api::path::home_dir().unwrap())
-                .join(".lettura")
-                .join("lettura.db");
-
+                .join(".stocker")
+                .join("stocker.db");
             let database_url = database_url.to_str().clone().unwrap();
-
             SqliteConnection::establish(&database_url)
                 .expect(&format!("Error connecting to {}", &database_url))
         }
