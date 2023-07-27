2b9efac33bfb1df82c0d24e8ea9e28c491c7c007
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-07-27 10:59:03 +0000

    work on other inteties

--- src-tauri/src/reposotories/mod.rs
+++ src-tauri/src/reposotories/mod.rs
@@ -1,1 +1,8 @@
+pub mod client_repo;
+pub mod inventory_mvm_repo;
+pub mod invoice_item_repo;
+pub mod invoice_repo;
+pub mod order_item_repo;
+pub mod order_repo;
 pub mod product_repo;
+pub mod user_repo;
