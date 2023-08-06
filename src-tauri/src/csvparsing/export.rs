735e861dac2dd8b74c32d617403e789fa1e303c7
AbdelilahOu <abdelilah4dev@gmail.com>
2023-08-06 23:15:44 +0000

    work on exporting data

--- src-tauri/src/csvparsing/export.rs
+++ src-tauri/src/csvparsing/export.rs
@@ -1,15 +1,27 @@
 use tauri::api::process::{Command, CommandEvent};
 
-pub async fn export_db_csv() {
-    let (mut rx, mut child) = Command::new("sqlite3")
-        .args(["serve"])
+pub async fn export_db_csv(source_path: &str, output_path: &str, table: String) {
+    let (mut rx, child) = Command::new("sqlite3")
+        .args([
+            "-header",
+            "-csv",
+            source_path,
+            format!("select * from {};", table).as_str(),
+            ">",
+            output_path,
+        ])
         .spawn()
         .expect("Failed to spawn packaged node");
 
     while let Some(event) = rx.recv().await {
         if let CommandEvent::Stdout(line) = event {
             println!("{:?}", line);
-            child.write("message from Rust\n".as_bytes()).unwrap();
+            let child_is_dead = child.kill();
+            match child_is_dead {
+                Ok(()) => {}
+                Err(_e) => {}
+            }
+            break;
         }
     }
 }
