0acf091c343724c12d7fe8ef3872eda94add6a5a
AbdelilahOu <abdelilah4dev@gmail.com>
2023-08-06 13:23:05 +0000

    work on client models

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -1,7 +1,6 @@
-use super::schema::products;
+use super::schema::{clients, products};
 use diesel::sql_types::*;
 use serde::{Deserialize, Serialize};
-
 
 #[derive(Debug, Queryable, QueryableByName, Clone, Serialize, Deserialize, AsChangeset)]
 #[diesel(table_name = clients)]
@@ -9,28 +8,25 @@ pub struct Client {
     #[diesel(sql_type = Integer)]
     pub id: i32,
     #[diesel(sql_type = Text)]
-    pub name: String,
+    pub fullname: String,
+    #[diesel(sql_type = Text)]
+    pub email: String,
     #[diesel(sql_type = Text)]
     pub image: String,
     #[diesel(sql_type = Text)]
-    pub description: String,
-    #[diesel(sql_type = Float)]
-    pub price: f32,
-    #[diesel(sql_type = Float)]
-    pub tva: f32,
+    pub address: String,
+    #[diesel(sql_type = Text)]
+    pub phone: String,
 }
 
 #[derive(Debug, Insertable, Clone, Serialize, Deserialize)]
 #[diesel(table_name = clients)]
 pub struct NewClient {
-    pub description: String,
-    pub name: String,
-    pub price: f32,
-    pub tva: f32,
+    pub fullname: String,
+    pub image: String,
+    pub address: String,
+    pub phone: String,
 }
-
-
-
 
 #[derive(Debug, Queryable, QueryableByName, Clone, Serialize, Deserialize, AsChangeset)]
 #[diesel(table_name = products)]
