6a07b8f5a9ee2a33ad0f30f30f211396dcf409c5
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-08-06 23:15:29 +0000

    work on exporting data

--- src-tauri/migrations/2022-10-19-055104_create_channels/down.sql
+++
@@ -1,9 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS clients;
-DROP TABLE IF EXISTS sellers;
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS invoices;
-DROP TABLE IF EXISTS order_items;
-DROP TABLE IF EXISTS invoice_items;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS stock_mouvements;
