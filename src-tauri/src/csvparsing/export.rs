b6994859e1e802e48e0842f09a8de51f07bfa6e2
AbdelilahOu <abdelilah4dev@gmail.com>
2023-08-07 11:43:56 +0000

    export data works

--- src-tauri/src/csvparsing/export.rs
+++ src-tauri/src/csvparsing/export.rs
@@ -1,27 +1,35 @@
+use std::fs::OpenOptions;
 use tauri::api::process::{Command, CommandEvent};
 
-pub async fn export_db_csv(source_path: &str, output_path: &str, table: String) {
-    let (mut rx, child) = Command::new("sqlite3")
-        .args([
-            "-header",
-            "-csv",
-            source_path,
-            format!("select * from {};", table).as_str(),
-            ">",
-            output_path,
-        ])
+pub async fn export_db_csv(source_path: &str, output_path: &str, _table: &String) {
+    // get data from command line
+    let (mut rx, _child) = Command::new("sqlite3")
+        .args(["-header", "-csv", source_path, "select * from products;"])
         .spawn()
         .expect("Failed to spawn packaged node");
-
+    // specify file options
+    let file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(output_path)
+        .unwrap();
+    // get file writer
+    let mut wtr = csv::Writer::from_writer(file);
+    // write to the file
     while let Some(event) = rx.recv().await {
         if let CommandEvent::Stdout(line) = event {
-            println!("{:?}", line);
-            let child_is_dead = child.kill();
-            match child_is_dead {
-                Ok(()) => {}
-                Err(_e) => {}
+            let write_record_result = wtr.write_record(
+                &line
+                    .split(r#"""#)
+                    .collect::<String>()
+                    .split(",")
+                    .collect::<Vec<&str>>(),
+            );
+            match write_record_result {
+                Ok(()) => println!("record written"),
+                Err(e) => println!("{:?}", e),
             }
-            break;
         }
     }
 }
