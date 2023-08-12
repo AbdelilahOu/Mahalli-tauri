ba9b7e26d45d78830eea0ec94aec69d43751e6cc
AbdelilahOu <abdelilah4dev@gmail.com>
2023-08-12 17:10:51 +0000

    renaming some stuff

--- src-tauri/src/reposotories/client_repo.rs
+++ src-tauri/src/reposotories/client_repo.rs
@@ -19,10 +19,10 @@ pub fn get_client(c_id: i32, connection: &mut SqliteConnection) -> Client {
 
     result
 }
-pub fn insert_client(new_client: NewClient, connection: &mut SqliteConnection) -> usize {
+pub fn insert_client(new_c: NewClient, connection: &mut SqliteConnection) -> usize {
     // let mut connection = establish_connection();
     let result = diesel::insert_into(schema::clients::dsl::clients)
-        .values(new_client)
+        .values(new_c)
         .execute(connection)
         .expect("Expect add articles");
 
@@ -37,15 +37,15 @@ pub fn delete_client(c_id: i32, connection: &mut SqliteConnection) -> usize {
 
     result
 }
-pub fn update_client(to_be_updated: Client, c_id: i32, connection: &mut SqliteConnection) -> usize {
+pub fn update_client(c_update: Client, c_id: i32, connection: &mut SqliteConnection) -> usize {
     // let mut connection = establish_connection();
 
     let result = diesel::update(schema::clients::dsl::clients.find(&c_id))
         .set((
-            schema::clients::fullname.eq(to_be_updated.fullname),
-            schema::clients::email.eq(to_be_updated.email),
-            schema::clients::address.eq(to_be_updated.address),
-            schema::clients::image.eq(to_be_updated.image),
+            schema::clients::fullname.eq(c_update.fullname),
+            schema::clients::email.eq(c_update.email),
+            schema::clients::address.eq(c_update.address),
+            schema::clients::image.eq(c_update.image),
         ))
         .execute(connection)
         .expect("Expect add articles");
