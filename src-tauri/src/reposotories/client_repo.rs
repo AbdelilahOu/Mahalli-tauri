caf4cec915a89291e900c7c03147c7f485375218
AbdelilahOu <abdelilah4dev@gmail.com>
2023-08-12 13:47:16 +0000

    use state in client crud

--- src-tauri/src/reposotories/client_repo.rs
+++ src-tauri/src/reposotories/client_repo.rs
@@ -1,4 +1,3 @@
-use crate::db::establish_connection;
 use crate::diesel::prelude::*;
 use crate::models::{Client, NewClient};
 use crate::schema;
@@ -11,44 +10,44 @@ pub fn get_clients(connection: &mut SqliteConnection) -> Vec<Client> {
     result
 }
 
-pub fn get_client(product_id: i32) -> Client {
-    let mut connection = establish_connection();
+pub fn get_client(c_id: i32, connection: &mut SqliteConnection) -> Client {
+    // let mut connection = establish_connection();
     let result = schema::clients::dsl::clients
-        .find(&product_id)
-        .first::<Client>(&mut connection)
+        .find(&c_id)
+        .first::<Client>(connection)
         .expect("error get all clients");
 
     result
 }
-pub fn insert_client(new_client: NewClient) -> usize {
-    let mut connection = establish_connection();
+pub fn insert_client(new_client: NewClient, connection: &mut SqliteConnection) -> usize {
+    // let mut connection = establish_connection();
     let result = diesel::insert_into(schema::clients::dsl::clients)
         .values(new_client)
-        .execute(&mut connection)
+        .execute(connection)
         .expect("Expect add articles");
 
     result
 }
 
-pub fn delete_client(product_id: i32) -> usize {
-    let mut connection = establish_connection();
-    let result = diesel::delete(schema::clients::dsl::clients.find(&product_id))
-        .execute(&mut connection)
+pub fn delete_client(c_id: i32, connection: &mut SqliteConnection) -> usize {
+    // let mut connection = establish_connection();
+    let result = diesel::delete(schema::clients::dsl::clients.find(&c_id))
+        .execute(connection)
         .expect("Expect delete channel");
 
     result
 }
-pub fn update_client(to_be_updated: Client, product_id: i32) -> usize {
-    let mut connection = establish_connection();
+pub fn update_client(to_be_updated: Client, c_id: i32, connection: &mut SqliteConnection) -> usize {
+    // let mut connection = establish_connection();
 
-    let result = diesel::update(schema::clients::dsl::clients.find(&product_id))
+    let result = diesel::update(schema::clients::dsl::clients.find(&c_id))
         .set((
             schema::clients::fullname.eq(to_be_updated.fullname),
             schema::clients::email.eq(to_be_updated.email),
             schema::clients::address.eq(to_be_updated.address),
             schema::clients::image.eq(to_be_updated.image),
         ))
-        .execute(&mut connection)
+        .execute(connection)
         .expect("Expect add articles");
 
     result
