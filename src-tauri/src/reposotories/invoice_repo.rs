6c0ca1cea6e86624bb44e7a2abcf0313822bb1cd
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-08-13 15:27:42 +0000

    cleanup

--- src-tauri/src/reposotories/invoice_repo.rs
+++ src-tauri/src/reposotories/invoice_repo.rs
@@ -1,59 +1,48 @@
-use crate::db::establish_connection;
 use crate::diesel::prelude::*;
 use crate::models::{Invoice, NewInvoice};
 use crate::schema;
 
 pub fn get_invoices(connection: &mut SqliteConnection) -> Vec<Invoice> {
-    let mut connection = establish_connection();
     let result = schema::invoices::dsl::invoices
-        .load::<Invoice>(&mut connection)
+        .load::<Invoice>(connection)
         .expect("Error fetching all invoices");
     result
 }
 
-pub fn get_invoice(invoice_id: i32, connection: &mut SqliteConnection) -> Invoice {
-    let mut connection = establish_connection();
+pub fn get_invoice(i_id: i32, connection: &mut SqliteConnection) -> Invoice {
     let result = schema::invoices::dsl::invoices
-        .find(&invoice_id)
-        .first::<Invoice>(&mut connection)
+        .find(&i_id)
+        .first::<Invoice>(connection)
         .expect("Error fetching invoice");
 
     result
 }
 
-pub fn insert_invoice(new_invoice: NewInvoice, connection: &mut SqliteConnection) -> usize {
-    let mut connection = establish_connection();
+pub fn insert_invoice(new_i: NewInvoice, connection: &mut SqliteConnection) -> usize {
     let result = diesel::insert_into(schema::invoices::dsl::invoices)
-        .values(new_invoice)
-        .execute(&mut connection)
+        .values(new_i)
+        .execute(connection)
         .expect("Error adding invoice");
 
     result
 }
 
-pub fn delete_invoice(invoice_id: i32, connection: &mut SqliteConnection) -> usize {
-    let mut connection = establish_connection();
-    let result = diesel::delete(schema::invoices::dsl::invoices.find(&invoice_id))
-        .execute(&mut connection)
+pub fn delete_invoice(i_id: i32, connection: &mut SqliteConnection) -> usize {
+    let result = diesel::delete(schema::invoices::dsl::invoices.find(&i_id))
+        .execute(connection)
         .expect("Error deleting invoice");
 
     result
 }
 
-pub fn update_invoice(
-    to_be_updated: Invoice,
-    invoice_id: i32,
-    connection: &mut SqliteConnection,
-) -> usize {
-    let mut connection = establish_connection();
-
-    let result = diesel::update(schema::invoices::dsl::invoices.find(&invoice_id))
+pub fn update_invoice(i_update: Invoice, i_id: i32, connection: &mut SqliteConnection) -> usize {
+    let result = diesel::update(schema::invoices::dsl::invoices.find(&i_id))
         .set((
-            schema::invoices::total.eq(to_be_updated.total),
-            schema::invoices::status.eq(to_be_updated.status),
+            schema::invoices::total.eq(i_update.total),
+            schema::invoices::status.eq(i_update.status),
             // Add other fields here if needed
         ))
-        .execute(&mut connection)
+        .execute(connection)
         .expect("Error updating invoice");
 
     result
