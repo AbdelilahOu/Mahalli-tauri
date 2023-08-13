48fbe5856ec545a0fb2f78b2c56ab47099986aff
AbdelilahOu <abdelilah4dev@gmail.com>
2023-08-13 15:55:44 +0000

    clean up

--- src-tauri/src/reposotories/user_repo.rs
+++ src-tauri/src/reposotories/user_repo.rs
@@ -4,42 +4,37 @@ use crate::models::{NewUser, User};
 use crate::schema;
 
 pub fn get_user(u_id: i32, connection: &mut SqliteConnection) -> User {
-    let mut connection = establish_connection();
     let result = schema::users::dsl::users
         .find(&u_id)
-        .first::<User>(&mut connection)
+        .first::<User>(connection)
         .expect("error get all users");
 
     result
 }
 pub fn insert_user(new_user: NewUser, connection: &mut SqliteConnection) -> usize {
-    let mut connection = establish_connection();
     let result = diesel::insert_into(schema::users::dsl::users)
         .values(new_user)
-        .execute(&mut connection)
+        .execute(connection)
         .expect("Expect add articles");
 
     result
 }
 
 pub fn delete_user(u_id: i32, connection: &mut SqliteConnection) -> usize {
-    let mut connection = establish_connection();
     let result = diesel::delete(schema::users::dsl::users.find(&u_id))
-        .execute(&mut connection)
+        .execute(connection)
         .expect("Expect delete channel");
 
     result
 }
 
 pub fn update_user(u_update: User, u_id: i32, connection: &mut SqliteConnection) -> usize {
-    let mut connection = establish_connection();
-
     let result = diesel::update(schema::users::dsl::users.find(&u_id))
         .set((
             schema::users::username.eq(u_update.username),
             schema::users::password.eq(u_update.password),
         ))
-        .execute(&mut connection)
+        .execute(connection)
         .expect("Expect add articles");
 
     result
