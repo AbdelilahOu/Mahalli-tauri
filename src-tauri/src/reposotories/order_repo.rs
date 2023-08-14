b6411a0ab8c6f6599a19c0c10aa217c2981ce529
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-08-14 11:53:24 +0000

    work on order items

--- src-tauri/src/reposotories/order_repo.rs
+++ src-tauri/src/reposotories/order_repo.rs
@@ -14,35 +14,35 @@ pub fn get_orders(page: i32, connection: &mut SqliteConnection) -> Vec<Order> {
     result
 }
 
-pub fn get_order(i_id: i32, connection: &mut SqliteConnection) -> Order {
+pub fn get_order(o_id: i32, connection: &mut SqliteConnection) -> Order {
     let result = schema::orders::dsl::orders
-        .find(&i_id)
+        .find(&o_id)
         .first::<Order>(connection)
         .expect("Error fetching order");
 
     result
 }
 
-pub fn insert_order(new_i: NewOrder, connection: &mut SqliteConnection) -> usize {
+pub fn insert_order(new_o: NewOrder, connection: &mut SqliteConnection) -> usize {
     let result = diesel::insert_into(schema::orders::dsl::orders)
-        .values(new_i)
+        .values(new_o)
         .execute(connection)
         .expect("Error adding order");
 
     result
 }
 
-pub fn delete_order(i_id: i32, connection: &mut SqliteConnection) -> usize {
-    let result = diesel::delete(schema::orders::dsl::orders.find(&i_id))
+pub fn delete_order(o_id: i32, connection: &mut SqliteConnection) -> usize {
+    let result = diesel::delete(schema::orders::dsl::orders.find(&o_id))
         .execute(connection)
         .expect("Error deleting order");
 
     result
 }
 
-pub fn update_order(i_update: Order, i_id: i32, connection: &mut SqliteConnection) -> usize {
-    let result = diesel::update(schema::orders::dsl::orders.find(&i_id))
-        .set(schema::orders::status.eq(i_update.status))
+pub fn update_order(o_update: Order, o_id: i32, connection: &mut SqliteConnection) -> usize {
+    let result = diesel::update(schema::orders::dsl::orders.find(&o_id))
+        .set(schema::orders::status.eq(o_update.status))
         .execute(connection)
         .expect("Error updating order");
 
