6aa6e72195fc7aa0b11f634a04ad4001f75b3c16
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-08-17 15:21:14 +0000

    clean up

--- src-tauri/src/reposotories/order_item_repo.rs
+++ src-tauri/src/reposotories/order_item_repo.rs
@@ -1,12 +1,12 @@
 use crate::diesel::prelude::*;
 use crate::models::{NewOrderItem, OrderItem};
-use crate::schema;
+use crate::schema::{order_items, orders};
 
 pub fn get_order_items(page: i32, connection: &mut SqliteConnection) -> Vec<OrderItem> {
     let offset = (page - 1) * 17;
 
-    let result = schema::order_items::dsl::order_items
-        .order(schema::order_items::id.desc())
+    let result = order_items::dsl::order_items
+        .order(order_items::id.desc())
         .limit(17)
         .offset(offset as i64)
         .load::<OrderItem>(connection)
@@ -16,7 +16,7 @@ pub fn get_order_items(page: i32, connection: &mut SqliteConnection) -> Vec<Orde
 }
 
 pub fn insert_order_item(new_oi: NewOrderItem, connection: &mut SqliteConnection) -> usize {
-    let result = diesel::insert_into(schema::order_items::dsl::order_items)
+    let result = diesel::insert_into(order_items::dsl::order_items)
         .values(new_oi)
         .execute(connection)
         .expect("Error adding order");
@@ -25,7 +25,7 @@ pub fn insert_order_item(new_oi: NewOrderItem, connection: &mut SqliteConnection
 }
 
 // pub fn get_order_item_by_order_id(o_id: i32, connection: &mut SqliteConnection) -> OrderItem {
-//     let result = schema::order_items::dsl::order_items
+//     let result = order_items::dsl::order_items
 //         .load::<OrderItem>(connection)
 //         .expect("Error fetching all orders");
 
@@ -33,7 +33,7 @@ pub fn insert_order_item(new_oi: NewOrderItem, connection: &mut SqliteConnection
 // }
 
 pub fn delete_order_item(oi_id: i32, connection: &mut SqliteConnection) -> usize {
-    let result = diesel::delete(schema::orders::dsl::orders.find(&oi_id))
+    let result = diesel::delete(orders::dsl::orders.find(&oi_id))
         .execute(connection)
         .expect("Error deleting order");
 
@@ -45,8 +45,8 @@ pub fn update_order_item(
     oi_id: i32,
     connection: &mut SqliteConnection,
 ) -> usize {
-    let result = diesel::update(schema::order_items::dsl::order_items.find(&oi_id))
-        .set(schema::order_items::quantity.eq(oi_update.quantity))
+    let result = diesel::update(order_items::dsl::order_items.find(&oi_id))
+        .set(order_items::quantity.eq(oi_update.quantity))
         .execute(connection)
         .expect("Error updating order");
 
