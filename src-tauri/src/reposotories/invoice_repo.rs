a1f623e2a993240f88072053f8f19aab3aa39296
AbdelilahOu <abdelilah4dev@gmail.com>
2023-08-17 14:00:51 +0000

    fix typos

--- src-tauri/src/reposotories/invoice_repo.rs
+++ src-tauri/src/reposotories/invoice_repo.rs
@@ -63,13 +63,59 @@ pub fn get_invoices(page: i32, connection: &mut SqliteConnection) -> Vec<Value> 
         .collect::<Vec<_>>()
 }
 
-pub fn get_invoice(i_id: i32, connection: &mut SqliteConnection) -> Invoice {
-    let result = schema::invoices::dsl::invoices
-        .find(&i_id)
-        .first::<Invoice>(connection)
-        .expect("Error fetching invoice");
+pub fn get_invoice(i_id: i32, connection: &mut SqliteConnection) -> Value {
+    let result = schema::invoices::table
+        .inner_join(schema::clients::table.on(schema::invoices::client_id.eq(schema::clients::id)))
+        .filter(schema::invoices::id.eq(i_id))
+        .select((schema::invoices::all_columns, schema::clients::all_columns))
+        .load::<(Invoice, Client)>(connection)
+        .expect("Error fetching invoices with clients");
 
     result
+        .into_iter()
+        .map(|(invoice, client)| {
+            let invoice_items: Vec<(InvoiceItem, Product)> = schema::invoice_items::table
+                .inner_join(
+                    schema::products::table
+                        .on(schema::invoice_items::product_id.eq(schema::products::id)),
+                )
+                .select((
+                    schema::invoice_items::all_columns,
+                    schema::products::all_columns,
+                ))
+                .filter(schema::invoice_items::invoice_id.eq(invoice.id))
+                .load::<(InvoiceItem, Product)>(connection)
+                .expect("Error fetching invoice items with products");
+
+            let invoice_items_json = json!({
+                "invoiceItems": invoice_items.into_iter().map(|(item, product)| {
+                    json!({
+                        "id": item.id,
+                        "quantity": item.quantity,
+                        "product_id": item.product_id,
+                        "inventory_id": item.inventory_id,
+                        "product": {
+                            "id": product.id,
+                            "name": product.name,
+                            "price": product.price
+                        }
+                    })
+                }).collect::<Vec<_>>()
+            });
+
+            json!({
+                "id": invoice.id,
+                "status": invoice.status,
+                "created_at": invoice.created_at,
+                "client_id": invoice.client_id,
+                "client": {
+                    "id": client.id,
+                    "fullname": client.fullname
+                },
+                "invoiceItems": invoice_items_json["invoiceItems"]
+            })
+        })
+        .collect::<Value>()
 }
 
 pub fn insert_invoice(new_i: NewInvoice, connection: &mut SqliteConnection) -> usize {
