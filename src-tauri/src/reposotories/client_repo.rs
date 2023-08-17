0a4852a4c6f228233f3eb65927200b11de2b3dee
AbdelilahOu <abdelilah4dev@gmail.com>
2023-08-17 15:21:04 +0000

    clean up

--- src-tauri/src/reposotories/client_repo.rs
+++ src-tauri/src/reposotories/client_repo.rs
@@ -1,19 +1,12 @@
 use crate::diesel::prelude::*;
 use crate::models::{Client, NewClient};
-use crate::schema;
-
-// pub fn get_all_clients(connection: &mut SqliteConnection) -> Vec<Client> {
-//     let result = schema::clients::dsl::clients
-//         .load::<Client>(connection)
-//         .expect("error get all clients");
-//     result
-// }
+use crate::schema::clients;
 
 pub fn get_clients(page: i32, connection: &mut SqliteConnection) -> Vec<Client> {
     let offset = (page - 1) * 17;
 
-    let result = schema::clients::dsl::clients
-        .order(schema::clients::id.desc())
+    let result = clients::dsl::clients
+        .order(clients::id.desc())
         .limit(17)
         .offset(offset as i64)
         .load::<Client>(connection)
@@ -22,7 +15,7 @@ pub fn get_clients(page: i32, connection: &mut SqliteConnection) -> Vec<Client> 
 }
 
 pub fn get_client(c_id: i32, connection: &mut SqliteConnection) -> Client {
-    let result = schema::clients::dsl::clients
+    let result = clients::dsl::clients
         .find(&c_id)
         .first::<Client>(connection)
         .expect("error get all clients");
@@ -32,13 +25,13 @@ pub fn get_client(c_id: i32, connection: &mut SqliteConnection) -> Client {
 
 pub fn insert_client(new_c: NewClient, connection: &mut SqliteConnection) -> Client {
     // insert
-    diesel::insert_into(schema::clients::dsl::clients)
+    diesel::insert_into(clients::dsl::clients)
         .values(new_c)
         .execute(connection)
         .expect("Expect add client");
     // select last inserted row
-    let result = schema::clients::dsl::clients
-        .order_by(schema::clients::id.desc())
+    let result = clients::dsl::clients
+        .order_by(clients::id.desc())
         .first::<Client>(connection)
         .expect("error get all clients");
 
@@ -46,7 +39,7 @@ pub fn insert_client(new_c: NewClient, connection: &mut SqliteConnection) -> Cli
 }
 
 pub fn delete_client(c_id: i32, connection: &mut SqliteConnection) -> usize {
-    let result = diesel::delete(schema::clients::dsl::clients.find(&c_id))
+    let result = diesel::delete(clients::dsl::clients.find(&c_id))
         .execute(connection)
         .expect("Expect delete client");
 
@@ -54,12 +47,12 @@ pub fn delete_client(c_id: i32, connection: &mut SqliteConnection) -> usize {
 }
 
 pub fn update_client(c_update: Client, c_id: i32, connection: &mut SqliteConnection) -> Client {
-    diesel::update(schema::clients::dsl::clients.find(&c_id))
+    diesel::update(clients::dsl::clients.find(&c_id))
         .set((
-            schema::clients::fullname.eq(c_update.fullname),
-            schema::clients::email.eq(c_update.email),
-            schema::clients::address.eq(c_update.address),
-            schema::clients::image.eq(c_update.image),
+            clients::fullname.eq(c_update.fullname),
+            clients::email.eq(c_update.email),
+            clients::address.eq(c_update.address),
+            clients::image.eq(c_update.image),
         ))
         .execute(connection)
         .expect("Expect add client");
