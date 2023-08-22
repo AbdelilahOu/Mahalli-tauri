a185f20ee111316aa95ab47e4ea7116be571d033
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-08-22 15:06:19 +0000

    add types

--- src-tauri/src/types.rs
+++ src-tauri/src/types.rs
@@ -1,13 +1,29 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
 pub struct TNewOrder {
     pub status: String,
     pub seller_id: i32,
-    pub order_items: Vec<TNewOrderItems>,
+    pub order_items: Vec<TNewOrderItem>,
 }
-
-pub struct TNewOrderItems {
+#[derive(Debug, Deserialize)]
+pub struct TNewOrderItem {
     pub product_id: i32,
     pub order_id: i32,
     pub quantity: i64,
     pub price: Option<f32>,
+    pub inventory_id: i32,
+}
+#[derive(Debug, Deserialize)]
+pub struct TNewInvoice {
+    pub status: String,
+    pub client_id: i32,
+    pub invoice_items: Vec<TNewInvoiceItem>,
+}
+#[derive(Debug, Deserialize)]
+pub struct TNewInvoiceItem {
+    pub product_id: i32,
+    pub invoice_id: i32,
+    pub quantity: i64,
     pub inventory_id: i32,
 }
