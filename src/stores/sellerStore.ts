80e7f8c059180a6aabe5c9c95413d2734f5aedf2
AbdelilahOu <abdelilah4dev@gmail.com>
2023-08-22 15:34:51 +0000

    stores clean up

--- src/stores/sellerStore.ts
+++ src/stores/sellerStore.ts
@@ -12,9 +12,6 @@ export const useSellerStore = defineStore("SellerStore", {
   actions: {
     getAllSellers: async function () {
       try {
-        this.sellers = await this.db.select<sellerT[]>(
-          "SELECT *  FROM sellers ORDER BY id DESC"
-        );
       } catch (error) {
         console.log(error);
       }
@@ -24,11 +21,6 @@ export const useSellerStore = defineStore("SellerStore", {
         this.sellers.find((sell: sellerT) => sell.id === id) ?? null;
       if (!this.seller) {
         try {
-          const seller: sellerT = await this.db.select<sellerT>(
-            "SELECT * FROM sellers WHERE id = $1",
-            [id]
-          );
-          this.seller = seller;
         } catch (error) {
           console.log(error);
         }
@@ -37,31 +29,18 @@ export const useSellerStore = defineStore("SellerStore", {
     createOneSeller: async function (seller: newSellerT) {
       try {
         let image: string = await saveFile(seller.image as string, "Image");
-
-        await this.db.execute(
-          "INSERT INTO sellers (name,email,phone,address,image) VALUES ($1,$2,$3,$4,$5)",
-          [seller.name, seller.email, seller.phone, seller.address, image]
-        );
-        this.getAllSellers();
       } catch (error) {
         console.log(error);
       }
     },
     deleteOneSeller: async function (id: number) {
       try {
-        await this.db.execute("DELETE FROM sellers WHERE id = $1", [id]);
-        this.getAllSellers();
       } catch (error) {
         console.log(error);
       }
     },
     updateOneSeller: async function (id: number, seller: updateSellerT) {
       try {
-        await this.db.execute(
-          "UPDATE sellers SET name = $1,email = $2,phone = $3,address = $4 WHERE id = $5",
-          [seller.name, seller.email, seller.phone, seller.address, seller.id]
-        );
-        this.getAllSellers();
       } catch (error) {
         console.log(error);
       }
