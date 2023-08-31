50648f8e4af1385f24089dc30471f71583edd89f
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-08-31 10:03:31 +0000

    change style

--- src/views/IndexView.tsx
+++ src/views/IndexView.tsx
@@ -22,7 +22,7 @@ export const IndexView = defineComponent({
         class={`w-screen h-screen relative overflow-y-auto bg-white scrollbar-thin scrollbar-thumb-transparent  flex `}
       >
         <div
-          class={`transition-all sticky top-1 border-r-2 border-slate-100 z-50 flex justify-center duration-200 transform ${
+          class={`transition-all sticky top-0 border-r-2 border-slate-100 z-50 flex justify-center duration-200 transform ${
             IsCollapse.value ? "w-12 min-w-[48px]" : "w-52 min-w-[12rem]"
           }`}
         >
