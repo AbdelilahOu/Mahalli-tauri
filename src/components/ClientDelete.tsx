c143d5bd639cb17b4386883abe1f43dc24cf8ca4
AbdelilahOu <abdelilah4dev@gmail.com>
2023-08-31 09:59:01 +0000

    name -> fullname

--- src/components/ClientDelete.tsx
+++ src/components/ClientDelete.tsx
@@ -21,7 +21,7 @@ export const ClientDelete = defineComponent({
     return () => (
       <div class="w-1/2 h-fit rounded-md z-50 gap-3 flex flex-col bg-white p-2 min-w-[350px]">
         <h1 class="font-semibold text-lg text-gray-800 border-b-2 border-b-gray-500 pb-2 uppercase text-center">
-          {globalTranslate("Clients.delete.title")} {client.value?.name} ?
+          {globalTranslate("Clients.delete.title")} {client.value?.fullname} ?
         </h1>
         <div class="flex gap-2">
           <UiButton colorTheme="a" Click={() => deleteTheClient()}>
