ce93a8a2b63c4087ae6928781d18511f492ba8d2
AbdelilahOu <abdelilah4dev@gmail.com>
2023-09-03 15:41:19 +0000

    clean up pinia

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -6,3 +6,8 @@ export const formatDate = (theDate: string): string =>
     hour: "2-digit",
     minute: "2-digit",
   });
+
+export const getWeekDay = (i: number) =>
+  new Date(2023, 4, i).toLocaleDateString("en-us", {
+    weekday: "short",
+  });
