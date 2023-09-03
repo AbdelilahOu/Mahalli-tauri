1a4567545df2214fbd3eb6bcf2be80d837d40330
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-03 15:41:47 +0000

    clean up pinia

--- src/views/SellersView.tsx
+++ src/views/SellersView.tsx
@@ -1,32 +1,31 @@
 import { defineComponent, onBeforeMount, ref, Transition } from "vue";
 import { globalTranslate } from "@/utils/globalTranslate";
 import { SellersTable } from "@/components/SellersTable";
-import { useSellerStore } from "@/stores/sellerStore";
 import { UiButton } from "@/components/ui/UiButton";
 import { useModalStore } from "@/stores/modalStore";
 import { UiInput } from "@/components/ui/UiInput";
 import UiIcon from "@/components/ui/UiIcon.vue";
-import { storeToRefs } from "pinia";
+import { invoke } from "@tauri-apps/api";
+import type { sellerT } from "@/types";
 
 export const SellersView = defineComponent({
   name: "Sellers",
   components: { SellersTable, UiButton, UiInput, UiIcon },
   setup() {
     const modalStore = useModalStore();
-    const SellerStore = useSellerStore();
-    const { sellers } = storeToRefs(SellerStore);
-    //
+    const sellers = ref<sellerT[]>([]);
     const searchQuery = ref<string>("");
-    //
-    onBeforeMount(() => {
-      if (!sellers.value.length) SellerStore.getAllSellers();
+    onBeforeMount(async () => {
+      try {
+        const res = await invoke<sellerT[]>("get_sellers", { page: 1 });
+      } catch (error) {
+        console.log(error);
+      }
     });
-    //
     const updateModal = (name: string) => {
       modalStore.updateModal({ key: "show", value: true });
       modalStore.updateModal({ key: "name", value: name });
     };
-    //
 
     return () => (
       <main class="w-full h-full px-3">
