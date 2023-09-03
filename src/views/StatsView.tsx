be54541fe2a28dc3e35ec60167ec98b85dcc0d10
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-03 15:41:56 +0000

    clean up pinia

--- src/views/StatsView.tsx
+++ src/views/StatsView.tsx
@@ -4,17 +4,17 @@ import { globalTranslate } from "@/utils/globalTranslate";
 import { chartOptions } from "@/constants/chartOptions";
 import { ChartHolder } from "@/components/ChartHolder";
 import { generateColor } from "@/utils/generateColor";
-import type { FilteredInventoryData } from "@/types";
-import { useStatsStore } from "@/stores/statsStore";
+import type { FilteredInventoryData, inOutReType } from "@/types";
 import { ChartLine } from "@/components/ChartLine";
 import { ChartBar } from "@/components/ChartBar";
+import { invoke } from "@tauri-apps/api";
+import _ from "lodash";
+import { getWeekDay } from "@/utils/formatDate";
 
 export const StatsView = defineComponent({
   name: "Stats",
   components: { ChartBar, ChartLine },
   setup() {
-    const statsStore = useStatsStore();
-
     const InsOuts = reactive({
       keys: ["IN", "OUT"] as const,
       months: [] as string[],
@@ -32,13 +32,117 @@ export const StatsView = defineComponent({
       },
     });
 
+    async function getInventoryMouvementStats() {
+      const results = new Map<string, { IN: number; OUT: number }>();
+      const months = new Set<string>();
+      //
+      const Rows: inOutReType = await invoke("get_inventory_stats");
+      //
+      for (const { group_month, total_in: IN, total_out: OUT } of Rows) {
+        const month = new Date(group_month).toLocaleDateString("en-us", {
+          month: "long",
+        });
+        months.add(month);
+        results.set(month, { IN, OUT: Math.abs(OUT) });
+      }
+      return {
+        // @ts-ignore
+        result: Object.fromEntries(results) as FilteredInventoryData,
+        months: Array.from(months),
+      };
+    }
+    async function getProductPerMonth(id: number, isClient = true) {
+      const data: any[] = await invoke(
+        isClient ? "get_c_product_month" : "get_s_product_month",
+        { id }
+      );
+
+      const existingDates = _.keys(_.groupBy(data, "month"));
+      const existingProducts = _.keys(_.groupBy(data, "name"));
+      const dataPerProduct = _.mapValues(_.groupBy(data, "name"), (value) =>
+        _.reduce(
+          value,
+          (pr, cr) => {
+            if (!pr) pr = [];
+            pr.push(cr.quantity);
+            return pr;
+          },
+          [] as number[]
+        )
+      );
+
+      return {
+        data: dataPerProduct,
+        dates: existingDates,
+        products: existingProducts,
+      };
+    }
+    async function getBestThree(isClients = true) {
+      const data: { name: string; amount: number }[] = await invoke(
+        isClients ? "get_b3_clients" : "get_b3_sellers"
+      );
+      //
+      const result = _.mapValues(_.groupBy(data, "name"), (value) =>
+        _.reduce(value, (pr, cr) => (pr += cr.amount), 0)
+      );
+      //
+      return { names: _.keys(result), result: _.values(result) };
+    }
+    async function getDailyExpenses(id: number, isClient = true) {
+      const result: { day: string; expense: number }[] = await invoke(
+        isClient ? "get_c_week_expenses" : "get_s_week_expenses",
+        { id }
+      );
+
+      console.log(
+        result,
+        isClient,
+        isClient ? "get_c_week_expenses" : "get_s_week_expenses"
+      );
+      // date related
+      const nextDay = new Date().getDay() == 6 ? 0 : new Date().getDay() + 1;
+      const resultMap = new Map<string, number>();
+      const weekDays = [0, 1, 2, 3, 4, 5, 6];
+
+      for (const index of weekDays) {
+        resultMap.set(getWeekDay(index), 0);
+      }
+
+      for (const { day, expense } of result) {
+        console.log(
+          day,
+          new Date(day).toLocaleDateString("en-us", {
+            weekday: "short",
+          })
+        );
+        resultMap.set(
+          new Date(day).toLocaleDateString("en-us", {
+            weekday: "short",
+          }),
+          expense
+        );
+      }
+
+      // @ts-ignore
+      const K = _.keys(Object.fromEntries(resultMap));
+      // @ts-ignore
+      const V = _.values(Object.fromEntries(resultMap));
+      const rearrangedKeys = K.slice(nextDay).concat(K.slice(0, nextDay));
+      const rearrangedValues = V.slice(nextDay).concat(V.slice(0, nextDay));
+
+      return {
+        keys: rearrangedKeys,
+        values: rearrangedValues,
+      };
+    }
+
     onBeforeMount(async () => {
-      const InOutStats = await statsStore.getInventoryMouvementStats();
+      const InOutStats = await getInventoryMouvementStats();
       InsOuts.months = InOutStats.months.reverse();
       InsOuts.data = InOutStats.result;
 
-      const TopClients = await statsStore.getBestThree();
-      const TopSellers = await statsStore.getBestThree(false);
+      const TopClients = await getBestThree();
+      const TopSellers = await getBestThree(false);
 
       BestThree.client.keys = TopClients.names;
       BestThree.client.data = TopClients.result;
