61d8d68fc5458a3607c2ac99c36a1aacf124846e
AbdelilahOu <abdelilah4dev@gmail.com>
2023-09-04 16:04:50 +0000

    migrate from pinia

--- src/components/SellersTable.tsx
+++ src/components/SellersTable.tsx
@@ -60,76 +60,71 @@ export const SellersTable = defineComponent({
                 .toLocaleLowerCase()
                 .includes(props.FilterParam)
             )
-              .slice(pagination.value * 17, pagination.value * 17 + 17)
-              .map((Seller, index) => (
-                <tr v-fade={index} key={Seller.id}>
-                  <td class="p-2">
-                    <span class="h-full w-full grid">
-                      <UiCheckBox
-                        onCheck={(check) => checkThisUser(check, Seller.id)}
+            .map((Seller, index) => (
+              <tr v-fade={index} key={Seller.id}>
+                <td class="p-2">
+                  <span class="h-full w-full grid">
+                    <UiCheckBox
+                      onCheck={(check) => checkThisUser(check, Seller.id)}
+                    />
+                  </span>
+                </td>
+                <td class="p-2">
+                  <div class="w-12 h-12 rounded-full overflow-hidden">
+                    {Seller.image && Seller.image !== "" ? (
+                      <img
+                        class="rounded-full w-full h-full object-cover"
+                        src={convertFileSrc(Seller.image)}
                       />
+                    ) : (
+                      <span class=" rounded-full w-full h-full object-fill animate-pulse bg-slate-300 duration-150" />
+                    )}
+                  </div>
+                </td>
+                <td class="p-2">
+                  <div class="font-medium text-gray-800">{Seller.name}</div>
+                </td>
+                <td class="p-2">
+                  <div class="text-left whitespace-nowrap overflow-ellipsis">
+                    {Seller.email ?? <span class="text-red-400">No email</span>}
+                  </div>
+                </td>
+                <td class="p-2">
+                  <div class="text-left whitespace-nowrap overflow-ellipsis">
+                    {Seller.phone ?? <span class="text-red-400">No phone</span>}
+                  </div>
+                </td>
+                <td class="p-2">
+                  <div class="text-left whitespace-nowrap overflow-ellipsis">
+                    {Seller.address ?? (
+                      <span class="text-red-400">No address</span>
+                    )}
+                  </div>
+                </td>
+                <td class="p-2">
+                  <div class="flex  w-full justify-start gap-3">
+                    <span
+                      onClick={() => toggleThisSeller(Seller, "SellerDelete")}
+                    >
+                      <UiIcon name={"delete"} />
                     </span>
-                  </td>
-                  <td class="p-2">
-                    <div class="w-12 h-12 rounded-full overflow-hidden">
-                      {Seller.image && Seller.image !== "" ? (
-                        <img
-                          class="rounded-full w-full h-full object-cover"
-                          src={convertFileSrc(Seller.image)}
-                        />
-                      ) : (
-                        <span class=" rounded-full w-full h-full object-fill animate-pulse bg-slate-300 duration-150" />
-                      )}
-                    </div>
-                  </td>
-                  <td class="p-2">
-                    <div class="font-medium text-gray-800">{Seller.name}</div>
-                  </td>
-                  <td class="p-2">
-                    <div class="text-left whitespace-nowrap overflow-ellipsis">
-                      {Seller.email ?? (
-                        <span class="text-red-400">No email</span>
-                      )}
-                    </div>
-                  </td>
-                  <td class="p-2">
-                    <div class="text-left whitespace-nowrap overflow-ellipsis">
-                      {Seller.phone ?? (
-                        <span class="text-red-400">No phone</span>
-                      )}
-                    </div>
-                  </td>
-                  <td class="p-2">
-                    <div class="text-left whitespace-nowrap overflow-ellipsis">
-                      {Seller.address ?? (
-                        <span class="text-red-400">No address</span>
-                      )}
-                    </div>
-                  </td>
-                  <td class="p-2">
-                    <div class="flex  w-full justify-start gap-3">
-                      <span
-                        onClick={() => toggleThisSeller(Seller, "SellerDelete")}
-                      >
-                        <UiIcon name={"delete"} />
-                      </span>
-                      <span
-                        onClick={() => toggleThisSeller(Seller, "SellerUpdate")}
-                      >
-                        <UiIcon name={"edit"} />
-                      </span>
-                      <RouterLink
-                        to={{
-                          name: "SellerDetails",
-                          params: { id: Seller.id },
-                        }}
-                      >
-                        <UiIcon name={"more"} />
-                      </RouterLink>
-                    </div>
-                  </td>
-                </tr>
-              ))}
+                    <span
+                      onClick={() => toggleThisSeller(Seller, "SellerUpdate")}
+                    >
+                      <UiIcon name={"edit"} />
+                    </span>
+                    <RouterLink
+                      to={{
+                        name: "SellerDetails",
+                        params: { id: Seller.id },
+                      }}
+                    >
+                      <UiIcon name={"more"} />
+                    </RouterLink>
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <div>
