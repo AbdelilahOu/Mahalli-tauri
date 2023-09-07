806d263297500934303f1a1c9243026747d6972e
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-07 09:33:54 +0000

    rearange imports

--- src/components/OrderDelete.tsx
+++ src/components/OrderDelete.tsx
@@ -1,9 +1,9 @@
 import { globalTranslate } from "@/utils/globalTranslate";
 import { defineComponent, onBeforeUnmount } from "vue";
 import { useModalStore } from "@/stores/modalStore";
-import { storeToRefs } from "pinia";
 import { UiButton } from "./ui/UiButton";
 import { invoke } from "@tauri-apps/api";
+import { storeToRefs } from "pinia";
 
 export const OrderDelete = defineComponent({
   name: "OrderDelete",
