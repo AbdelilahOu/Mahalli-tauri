87c572b0ec3d394c6902ff1556a5d5e39a235934
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-10 22:44:28 +0000

    use global store

--- src/components/OrderDelete.tsx
+++ src/components/OrderDelete.tsx
@@ -1,10 +1,11 @@
 import { globalTranslate } from "@/utils/globalTranslate";
-import { defineComponent, onBeforeUnmount } from "vue";
-import { useModalStore } from "@/stores/modalStore";
+import { computed, defineComponent, onBeforeUnmount } from "vue";
+import { store } from "@/store";
 import { UiButton } from "./ui/UiButton";
 import { invoke } from "@tauri-apps/api";
-import { storeToRefs } from "pinia";
+
 import { useRoute, useRouter } from "vue-router";
+import type { orderT } from "@/types";
 
 export const OrderDelete = defineComponent({
   name: "OrderDelete",
@@ -13,8 +14,8 @@ export const OrderDelete = defineComponent({
     const route = useRoute();
     const router = useRouter();
     //
-    const modalStore = useModalStore();
-    const { order } = storeToRefs(modalStore);
+
+    const order = computed(() => store.getters.getSelectedRow<orderT>());
     //
     const updateQueryParams = (query: Record<any, any>) => {
       router.push({
@@ -29,16 +30,21 @@ export const OrderDelete = defineComponent({
       if (id) {
         try {
           await invoke("delete_order", { id });
-          updateQueryParams({ refresh: "refresh-delete" });
+          // toggle refresh
+          updateQueryParams({
+            refresh: "refresh-delete-" + Math.random() * 9999,
+          });
         } catch (error) {
           console.log(error);
         } finally {
-          modalStore.updateModal({ key: "show", value: false });
+          store.setters.updateStore({ key: "show", value: false });
         }
       }
     };
     //
-    onBeforeUnmount(() => modalStore.updateOrdersRow(null));
+    onBeforeUnmount(() =>
+      store.setters.updateStore({ key: "row", value: null })
+    );
     return () => (
       <div class="w-1/2 h-fit rounded-md z-50 gap-3 flex flex-col bg-white p-2 min-w-[350px]">
         <h1 class="font-semibold text-lg text-gray-800 border-b-2 border-b-gray-500 pb-2 uppercase text-center">
@@ -49,7 +55,9 @@ export const OrderDelete = defineComponent({
             {globalTranslate("Orders.delete.yes")}
           </UiButton>
           <UiButton
-            Click={() => modalStore.updateModal({ key: "show", value: false })}
+            Click={() =>
+              store.setters.updateStore({ key: "show", value: false })
+            }
           >
             {globalTranslate("Orders.delete.no")}
           </UiButton>
