5e098c6a685a06767b27217bf4f89f150a119699
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-17 23:28:34 +0000

    default values

--- src/constants/defaultValues.ts
+++ src/constants/defaultValues.ts
@@ -79,3 +79,70 @@ export const SELLER_UPDATE = {
   address: undefined,
   image: undefined,
 };
+
+export const CHART_OPTIONS = {
+  responsive: true,
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+      ticks: {
+        color: "rgba(25,23,17,0.6)",
+        textStrokeWidth: 10,
+      },
+      border: {
+        display: false,
+      },
+    },
+    y: {
+      grid: {
+        lineWidth: 1,
+        drawBorder: false,
+      },
+      border: {
+        display: false,
+      },
+      ticks: {
+        color: "rgba(25,23,17,0.6)",
+        min: 0,
+        textStrokeWidth: 1,
+        padding: 10,
+      },
+    },
+  },
+};
+
+export const CHART_WO_TICKS = {
+  responsive: true,
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+      ticks: {
+        color: "rgba(25,23,17,0.6)",
+        textStrokeWidth: 10,
+      },
+      border: {
+        display: false,
+      },
+    },
+    y: {
+      grid: {
+        lineWidth: 0.5,
+        drawBorder: false,
+      },
+      border: {
+        display: false,
+      },
+      ticks: {
+        display: false,
+        // color: "rgba(25,23,17,0.6)",
+        // min: 0,
+        // textStrokeWidth: 1,
+        // padding: 10,
+      },
+    },
+  },
+};
