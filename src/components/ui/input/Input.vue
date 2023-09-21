087021ffcf205c4d3ee375ee2a38896e163370ed
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-21 12:58:45 +0000

    i dont know what im doing

---
+++ src/components/ui/input/Input.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useVModel } from "@vueuse/core";
+import { cn } from "@/lib/utils";
+
+const props = defineProps<{
+  defaultValue?: string | number;
+  modelValue?: string | number;
+  type: string;
+  placeHolder: string;
+}>();
+
+const emits = defineEmits<{
+  (e: "update:modelValue", payload: string | number): void;
+}>();
+
+const modelValue = useVModel(props, "modelValue", emits, {
+  passive: true,
+  defaultValue: props.defaultValue,
+});
+</script>
+
+<template>
+  <input
+    v-model="modelValue"
+    :type="type"
+    :placeholder="placeHolder"
+    :class="
+      cn(
+        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:border-black disabled:cursor-not-allowed disabled:opacity-50 transition-color duration-150',
+        $attrs.class ?? ''
+      )
+    "
+  />
+</template>
