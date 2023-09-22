1e23fc999993a25431e1f9a46626168d7045d3c4
AbdelilahOu <abdelilah4dev@gmail.com>
2023-09-22 17:45:30 +0000

    Xcomponent stuff

---
+++ src/components/Error.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-1/2 h-fit z-50 gap-3 flex flex-col bg-white p-2 min-w-[350px]">
+    <h1
+      class="font-semibold text-lg text-gray-800 border-b-2 border-b-gray-500 pb-2 uppercase text-center"
+    >
+      Error
+    </h1>
+    <div class="h-full w-full flex flex-col gap-2"></div>
+  </div>
+</template>
