76b44527556875c45d4f698419c11fa0ee4bde0c
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-09-25 12:27:53 +0000

    update models id as string

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -5,8 +5,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Queryable, QueryableByName, Clone, Serialize, Deserialize, AsChangeset)]
 #[diesel(table_name = users)]
 pub struct User {
-    #[diesel(sql_type = Integer)]
-    pub id: i32,
+    #[diesel(sql_type = Text)]
+    pub id: String,
     #[diesel(sql_type = Text)]
     pub username: String,
     #[diesel(sql_type = Text)]
@@ -20,6 +20,7 @@ pub struct User {
 #[derive(Debug, Insertable, Clone, Serialize, Deserialize)]
 #[diesel(table_name = users)]
 pub struct NewUser {
+    pub id: String,
     pub username: String,
     pub password: String,
     pub email: String,
@@ -29,12 +30,14 @@ pub struct NewUser {
 #[derive(Debug, Queryable, QueryableByName, Clone, Serialize, Deserialize, AsChangeset)]
 #[diesel(table_name = clients)]
 pub struct Client {
-    #[diesel(sql_type = Integer)]
-    pub id: i32,
+    #[diesel(sql_type = Text)]
+    pub id: String,
     #[diesel(sql_type = Text)]
     pub fullname: String,
     #[diesel(sql_type = Text)]
     pub phone: String,
+    #[diesel(sql_type = Text)]
+    pub created_at: String,
     #[diesel(sql_type = Text)]
     pub email: String,
     #[diesel(sql_type = Text)]
@@ -46,6 +49,7 @@ pub struct Client {
 #[derive(Debug, Insertable, Clone, Serialize, Deserialize)]
 #[diesel(table_name = clients)]
 pub struct NewClient {
+    pub id: String,
     pub fullname: String,
     pub email: String,
     pub phone: String,
@@ -56,12 +60,14 @@ pub struct NewClient {
 #[derive(Debug, Queryable, Clone, QueryableByName, Serialize, Deserialize, AsChangeset)]
 #[diesel(table_name = sellers)]
 pub struct Seller {
-    #[diesel(sql_type = Integer)]
-    pub id: i32,
+    #[diesel(sql_type = Text)]
+    pub id: String,
     #[diesel(sql_type = Text)]
     pub name: String,
     #[diesel(sql_type = Text)]
     pub phone: String,
+    #[diesel(sql_type = Text)]
+    pub created_at: String,
     #[diesel(sql_type = Text)]
     pub email: String,
     #[diesel(sql_type = Text)]
@@ -73,6 +79,8 @@ pub struct Seller {
 #[derive(Debug, Insertable, Clone, Serialize, Deserialize)]
 #[diesel(table_name = sellers)]
 pub struct NewSeller {
+    pub id: String,
+
     pub name: String,
     pub image: String,
     pub address: String,
@@ -83,14 +91,16 @@ pub struct NewSeller {
 #[derive(Debug, Queryable, QueryableByName, Clone, Serialize, Deserialize, AsChangeset)]
 #[diesel(table_name = products)]
 pub struct Product {
-    #[diesel(sql_type = Integer)]
-    pub id: i32,
+    #[diesel(sql_type = Text)]
+    pub id: String,
     #[diesel(sql_type = Text)]
     pub name: String,
     #[diesel(sql_type = Text)]
     pub image: String,
     #[diesel(sql_type = Text)]
     pub description: String,
+    #[diesel(sql_type = Text)]
+    pub created_at: String,
     #[diesel(sql_type = Float)]
     pub price: f32,
     #[diesel(sql_type = Float)]
@@ -100,6 +110,8 @@ pub struct Product {
 #[derive(Debug, Insertable, Clone, Serialize, Deserialize)]
 #[diesel(table_name = products)]
 pub struct NewProduct {
+    pub id: String,
+
     pub description: String,
     pub name: String,
     pub price: f32,
@@ -110,14 +122,14 @@ pub struct NewProduct {
 #[derive(Debug, Queryable, Deserialize, Selectable, Serialize, Associations, QueryableByName)]
 #[diesel(table_name = invoices, belongs_to(Client, foreign_key = client_id))]
 pub struct Invoice {
-    #[diesel(sql_type = Integer)]
-    pub id: i32,
+    #[diesel(sql_type = Text)]
+    pub id: String,
     #[diesel(sql_type = Text)]
     pub status: String,
     #[diesel(sql_type = Text)]
     pub created_at: String,
-    #[diesel(sql_type = Integer)]
-    pub client_id: i32,
+    #[diesel(sql_type = Text)]
+    pub client_id: String,
 }
 
 #[derive(Debug, Deserialize, AsChangeset, Serialize)]
@@ -129,8 +141,10 @@ pub struct UpdateInvoice {
 #[derive(Debug, Insertable, Clone, Serialize, Deserialize)]
 #[diesel(table_name = invoices)]
 pub struct NewInvoice {
+    pub id: String,
+
     pub status: String,
-    pub client_id: i32,
+    pub client_id: String,
 }
 
 #[derive(
@@ -138,16 +152,16 @@ pub struct NewInvoice {
 )]
 #[diesel(table_name = invoice_items, belongs_to(Product, foreign_key = product_id),belongs_to(Invoice, foreign_key = invoice_id),belongs_to(InventoryMvm, foreign_key = inventory_id))]
 pub struct InvoiceItem {
-    #[diesel(sql_type = Integer)]
-    pub id: i32,
-    #[diesel(sql_type = Integer)]
-    pub product_id: i32,
-    #[diesel(sql_type = Integer)]
-    pub invoice_id: i32,
+    #[diesel(sql_type = Text)]
+    pub id: String,
+    #[diesel(sql_type = Text)]
+    pub product_id: String,
+    #[diesel(sql_type = Text)]
+    pub invoice_id: String,
     #[diesel(sql_type = Integer)]
     pub quantity: i64,
-    #[diesel(sql_type = Integer)]
-    pub inventory_id: i32,
+    #[diesel(sql_type = Text)]
+    pub inventory_id: String,
 }
 
 #[derive(Debug, AsChangeset, Serialize, Deserialize)]
@@ -160,10 +174,11 @@ pub struct UpdateInvoiceItem {
 #[derive(Debug, Insertable, Clone, Serialize, Deserialize)]
 #[diesel(table_name = invoice_items)]
 pub struct NewInvoiceItem {
-    pub product_id: i32,
-    pub invoice_id: i32,
+    pub id: String,
+    pub product_id: String,
+    pub invoice_id: String,
     pub quantity: i64,
-    pub inventory_id: i32,
+    pub inventory_id: String,
 }
 
 #[derive(
@@ -171,14 +186,14 @@ pub struct NewInvoiceItem {
 )]
 #[diesel(table_name = orders, belongs_to(Seller, foreign_key = seller_id))]
 pub struct Order {
-    #[diesel(sql_type = Integer)]
-    pub id: i32,
+    #[diesel(sql_type = Text)]
+    pub id: String,
     #[diesel(sql_type = Text)]
     pub status: String,
     #[diesel(sql_type = Text)]
     pub created_at: String,
-    #[diesel(sql_type = Integer)]
-    pub seller_id: i32,
+    #[diesel(sql_type = Text)]
+    pub seller_id: String,
 }
 
 #[derive(Debug, AsChangeset, Clone, Serialize)]
@@ -190,8 +205,9 @@ pub struct UpdateOrder {
 #[derive(Debug, Insertable, Clone, Serialize, Deserialize)]
 #[diesel(table_name = orders)]
 pub struct NewOrder {
+    pub id: String,
     pub status: String,
-    pub seller_id: i32,
+    pub seller_id: String,
 }
 
 #[derive(
@@ -199,16 +215,16 @@ pub struct NewOrder {
 )]
 #[diesel(table_name = order_items, belongs_to(Product, foreign_key = product_id),belongs_to(Order, foreign_key = order_id),belongs_to(InventoryMvm, foreign_key = inventory_id))]
 pub struct OrderItem {
-    #[diesel(sql_type = Integer)]
-    pub id: i32,
-    #[diesel(sql_type = Integer)]
-    pub product_id: i32,
+    #[diesel(sql_type = Text)]
+    pub id: String,
+    #[diesel(sql_type = Text)]
+    pub product_id: String,
     #[diesel(sql_type = Nullable<Float>)]
     pub price: Option<f32>,
-    #[diesel(sql_type = Integer)]
-    pub order_id: i32,
-    #[diesel(sql_type = Integer)]
-    pub inventory_id: i32,
+    #[diesel(sql_type = Text)]
+    pub order_id: String,
+    #[diesel(sql_type = Text)]
+    pub inventory_id: String,
     #[diesel(sql_type = BigInt)]
     pub quantity: i64,
 }
@@ -225,26 +241,29 @@ pub struct UpdateOrderItem {
 #[derive(Debug, Insertable, Clone, Serialize, Deserialize)]
 #[diesel(table_name = order_items)]
 pub struct NewOrderItem {
-    pub product_id: i32,
-    pub order_id: i32,
+    pub id: String,
+    pub product_id: String,
+    pub order_id: String,
     pub quantity: i64,
     pub price: Option<f32>,
-    pub inventory_id: i32,
+    pub inventory_id: String,
 }
 
 #[derive(Debug, Queryable, QueryableByName, Clone, Serialize, Deserialize, Associations)]
 #[diesel(table_name = inventory_mouvements, belongs_to(Product, foreign_key = product_id))]
 pub struct InventoryMvm {
-    #[diesel(sql_type = Integer)]
-    pub id: i32,
+    #[diesel(sql_type = Text)]
+    pub id: String,
     #[diesel(sql_type = Text)]
     pub date: String,
     #[diesel(sql_type = Text)]
     pub model: String,
     #[diesel(sql_type = Float)]
     pub quantity: i64,
-    #[diesel(sql_type = Integer)]
-    pub product_id: i32,
+    #[diesel(sql_type = Text)]
+    pub created_at: String,
+    #[diesel(sql_type = Text)]
+    pub product_id: String,
 }
 
 #[derive(Debug, AsChangeset, Serialize, Deserialize)]
@@ -257,15 +276,16 @@ pub struct UpdateInventoryMvm {
 #[derive(Debug, Insertable, Clone, Serialize, Deserialize)]
 #[diesel(table_name = inventory_mouvements)]
 pub struct NewInventoryMvm {
+    pub id: String,
     pub model: String,
     pub quantity: i64,
-    pub product_id: i32,
+    pub product_id: String,
 }
 
 #[derive(QueryableByName, Queryable, Deserialize, Serialize)]
 pub struct ProductWithQuantity {
-    #[diesel(sql_type = Integer)]
-    pub id: i32,
+    #[diesel(sql_type = Text)]
+    pub id: String,
     #[diesel(sql_type = Text)]
     pub name: String,
     #[diesel(sql_type = Text)]
