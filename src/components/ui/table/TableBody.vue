3d7b39cc81e00cf532ae28a65b99715cbc7bd013
AbdelilahOu <abdelilah4dev@gmail.com>
2023-10-29 16:00:30 +0000

    add table modal

---
+++ src/components/ui/table/TableBody.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { cn } from '@/utils/shadcn'
+
+const props = defineProps<{ class?: string }>()
+</script>
+
+<template>
+  <tbody :class="cn('[&_tr:last-child]:border-0', props.class)">
+    <slot />
+  </tbody>
+</template>
