53c7db264d22286ef023ebd38ca49ff46c82a9e6
abdelilah ouaadouch <gxgamer710@gmail.com>
2023-10-30 11:23:54 +0000

    data table doesnt work

--- src/components/ui/dropdown-menu/DropdownMenuShortcut.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { cn } from '@/utils/shadcn'
-</script>
-
-<template>
-  <span :class="cn('ml-auto text-xs tracking-widest opacity-60', $attrs.class ?? '')">
-    <slot />
-  </span>
-</template>
