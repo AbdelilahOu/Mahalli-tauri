e873132ae3610f647c1507143b445abe58370a29
AbdelilahOu <abdelilah4dev@gmail.com>
2024-02-15 11:46:52 +0000

    FEAT: init schema

---
+++ src-tauri/migration/src/m20220101_000001_init_.rs
@@ -0,0 +1,98 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Client::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Client::Id).string().not_null().primary_key())
+                    .col(ColumnDef::new(Client::Fullname).string().not_null())
+                    .col(
+                        ColumnDef::new(Client::CreatedAt)
+                            .date_time()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .col(ColumnDef::new(Client::Phone).string())
+                    .col(ColumnDef::new(Client::Email).string())
+                    .col(ColumnDef::new(Client::Address).string())
+                    .col(ColumnDef::new(Client::Image).string())
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Seller::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Seller::Id).string().not_null().primary_key())
+                    .col(ColumnDef::new(Seller::Fullname).string().not_null())
+                    .col(
+                        ColumnDef::new(Seller::CreatedAt)
+                            .date_time()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .col(ColumnDef::new(Seller::Phone).string())
+                    .col(ColumnDef::new(Seller::Email).string())
+                    .col(ColumnDef::new(Seller::Address).string())
+                    .col(ColumnDef::new(Seller::Image).string())
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Client::Table).to_owned())
+            .await?;
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+enum Client {
+    #[sea_orm(iden = "clients")]
+    Table,
+    Id,
+    #[sea_orm(iden = "full_name")]
+    Fullname,
+    #[sea_orm(iden = "phone_number")]
+    Phone,
+    #[sea_orm(iden = "created_at")]
+    CreatedAt,
+    #[sea_orm(iden = "image")]
+    Image,
+    #[sea_orm(iden = "email")]
+    Email,
+    #[sea_orm(iden = "address")]
+    Address,
+}
+
+#[derive(DeriveIden)]
+enum Seller {
+    #[sea_orm(iden = "sellers")]
+    Table,
+    Id,
+    #[sea_orm(iden = "full_name")]
+    Fullname,
+    #[sea_orm(iden = "phone_number")]
+    Phone,
+    #[sea_orm(iden = "created_at")]
+    CreatedAt,
+    #[sea_orm(iden = "image")]
+    Image,
+    #[sea_orm(iden = "email")]
+    Email,
+    #[sea_orm(iden = "address")]
+    Address,
+}
