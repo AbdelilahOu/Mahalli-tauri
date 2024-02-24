462ce5ec45327139bc2cc4dd7878359cd6b52530
AbdelilahOu <abdelilah4dev@gmail.com>
2024-02-24 12:07:52 +0000

    FEAT: update SearchableItems

--- src/components/ui/UISearchableItems.vue
+++ src/components/ui/UISearchableItems.vue
@@ -7,8 +7,10 @@ import {
 import { Input } from "@/components/ui/input";
 import { ref } from "vue";
 import { cn } from "@/utils/shadcn";
-const open = ref(false);
+import { onClickOutside } from "@vueuse/core";
 
+const open = ref(false);
+const dropdownParent = ref<HTMLElement | null>(null);
 const props = defineProps<{
   defaultValue?: string;
   items: { label: string; value: string }[];
@@ -30,6 +32,8 @@ const updateInput = (s: string | number) => {
   }, 500);
 };
 
+onClickOutside(dropdownParent, () => (open.value = false));
+
 const selectItem = (item: { label: string; value: string }) => {
   inputValue.value = item.label;
   emits("on:select", item.value);
@@ -38,34 +42,47 @@ const selectItem = (item: { label: string; value: string }) => {
 </script>
 
 <template>
-  <Popover :open="open">
-    <PopoverTrigger aria-disabled="true">
-      <Input
-        v-model:model-value="inputValue"
-        @update:model-value="updateInput"
-        placeHolder="search"
-      />
-    </PopoverTrigger>
-    <PopoverContent @focus-outside="open = false">
-      <div
-        :class="
-          cn(
-            'space-y-1',
-            items.length > 5
-              ? 'max-h-60 overflow-auto scrollbar-thin scrollbar-thumb-transparent'
-              : 'h-fit',
-          )
-        "
+  <div ref="dropdownParent">
+    <Popover :open="open">
+      <PopoverTrigger class="w-full" aria-disabled="true">
+        <Input
+          v-model:model-value="inputValue"
+          @update:model-value="updateInput"
+          placeHolder="search"
+        />
+      </PopoverTrigger>
+      <PopoverContent
+        @open-auto-focus="(e) => e.preventDefault()"
+        @focus-outside="open = false"
       >
-        <span
-          class="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
-          v-for="item in items"
-          :key="item.value"
-          @click="() => selectItem(item)"
+        <div
+          v-if="items.length > 0"
+          :class="
+            cn(
+              'space-y-1',
+              items.length > 5
+                ? 'max-h-60 overflow-auto scrollbar-thin scrollbar-thumb-transparent'
+                : 'h-fit',
+            )
+          "
         >
-          {{ item.label }}
-        </span>
-      </div>
-    </PopoverContent>
-  </Popover>
+          <span
+            class="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+            v-for="item in items"
+            :key="item.value"
+            @click="() => selectItem(item)"
+          >
+            {{ item.label }}
+          </span>
+        </div>
+        <div v-else>
+          <span
+            class="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+          >
+            No elements
+          </span>
+        </div>
+      </PopoverContent>
+    </Popover>
+  </div>
 </template>
