3619b7672aa05bf23e0446c756f9991e578881ee
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-02-29 19:55:06 +0000

    FEAT: use unovis on charts

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -2,27 +2,30 @@
 import ChartHolder from "@/components/ChartHolder.vue";
 import { onBeforeMount } from "vue";
 import { useI18n } from "vue-i18n";
-import { VisXYContainer, VisGroupedBar, VisAxis } from "@unovis/vue";
+import {
+  VisXYContainer,
+  VisGroupedBar,
+  VisAxis,
+  VisStackedBar,
+} from "@unovis/vue";
 
 import { invoke } from "@tauri-apps/api";
 import { ref } from "vue";
-import type { DataRecord } from "@/types";
+import type { DataRecord, Res } from "@/types";
 
 const { t, d } = useI18n();
 
-const mouvements = ref<any[]>();
-const x = (d: any) => {
-  console.log(d);
-  return d.createdAt;
-};
-const y = [(d: any) => d.price, (d: any) => d.quantity];
+const mouvements = ref<any[]>([]);
+const mouvementsLabels = ref<string[]>([]);
 
 async function getInventoryMouvementStats() {
   try {
-    const res = await invoke<any>("list_mvm_stats");
+    const res = await invoke<Res<any[]>>("list_mvm_stats");
     if (!res?.error) {
-      console.log(res);
       mouvements.value = res.data;
+      let mouvementLabelsSet = new Set<string>();
+      res.data.forEach((a) => mouvementLabelsSet.add(a.createdAt));
+      mouvementsLabels.value = [...mouvementLabelsSet];
       return;
     }
   } catch (error) {
@@ -39,12 +42,34 @@ onBeforeMount(async () => {
   <main class="w-full h-full">
     <div class="w-full h-full flex flex-col gap-4">
       <div class="w-full h-fit">
-        <ChartHolder>
+        <ChartHolder v-if="mouvements">
           <template #default>
-            <VisXYContainer :height="500">
-              <VisGroupedBar :data="mouvements" :x="x" :y="y" />
-              <VisAxis type="x" label="Election Year" />
-              <VisAxis type="y" label="Number of Votes (millions)" />
+            <VisXYContainer :data="mouvements" :height="500">
+              <VisGroupedBar
+                :x="(_: any, index: number) => index"
+                :y="(d: any) => d.quantity / 100"
+              />
+              <VisAxis type="x" label="months" />
+              <VisAxis type="y" label="quantity 100 item" />
+            </VisXYContainer>
+          </template>
+          <template #title>
+            <h1 class="m-2 w-full text-center text-base font-medium">
+              <i>{{ t("dashboard.i.title") }}</i>
+            </h1>
+          </template>
+        </ChartHolder>
+      </div>
+      <div class="w-full h-fit">
+        <ChartHolder v-if="mouvements">
+          <template #default>
+            <VisXYContainer :data="mouvements" :height="500">
+              <VisGroupedBar
+                :x="(_: any, index: number) => index"
+                :y="(d: any) => d.price"
+              />
+              <VisAxis type="x" label="months" />
+              <VisAxis type="y" label="price in dh" />
             </VisXYContainer>
           </template>
           <template #title>
