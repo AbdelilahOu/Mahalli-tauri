f73f18e47d7859dbec34db541e7c3b1026e66106
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-03-06 12:21:25 +0000

    FEAT: update locales add new components

--- src/components/ModalLoading.vue
+++ src/components/ModalLoading.vue
@@ -3,7 +3,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 </script>
 
 <template>
-  <div class="w-1/2 h-fit z-50 gap-3 flex flex-col bg-white p-2 min-w-[350px]">
+  <div
+    class="w-1/2 h-fit z-50 gap-3 flex flex-col bg-white rounded-md p-2 min-w-[350px]"
+  >
     <h1
       class="font-semibold text-lg h-10 text-gray-800 border-b-2 border-b-gray-500 pb-2 uppercase text-center"
     >
