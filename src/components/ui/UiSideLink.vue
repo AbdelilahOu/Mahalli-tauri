e44b69a2d217088c8c6587089df81a25a3e489d8
AbdelilahOu <abdelilah4dev@gmail.com>
2024-03-06 16:39:36 +0000

    FEAT: fix styles

--- src/components/ui/UiSideLink.vue
+++ src/components/ui/UiSideLink.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import UiIcon from "./UiIcon.vue";
+import { cn } from "@/utils/shadcn";
 
 defineProps<{
   isText: boolean;
@@ -10,22 +11,24 @@ defineProps<{
 
 <template>
   <span
-    :class="[
-      'w-full flex h-9 rounded-md items-center py-1 px-2 hover:bg-white group transition-all duration-300',
-      isText ? 'justify-start' : 'justify-center',
-    ]"
+    :class="
+      cn(
+        'w-full flex h-9  rounded-md items-center py-1 px-1 hover:bg-white group transition-all duration-300',
+        isText ? 'gap-1' : '',
+      )
+    "
   >
     <UiIcon
       :isStyled="false"
-      extraStyle="min-w-[30px] h-[30px] w-[30px] max-w-[30px] group-hover:text-primary transition-all duration-150 text-gray-400"
+      extraStyle="min-w-[30px] max-w-[30px] group-hover:text-primary transition-all duration-150 text-gray-400"
       :name="icon"
     />
     <span
       v-if="isText"
-      class="text-gray-500 ml-1 whitespace-nowrap group-hover:text-primary"
+      class="text-gray-500 whitespace-nowrap text-sm group-hover:text-primary"
     >
       {{ linkText }}
     </span>
-    <span v-else class="w-full"></span>
+    <!-- <span v-else class="w-full"></span> -->
   </span>
 </template>
