2a7a8d91811a1e94f1037dc925bb610ead8f71fb
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-03-17 23:35:50 +0000

    FEAT: update profile

--- src/components/ClientProfile.vue
+++ src/components/ClientProfile.vue
@@ -8,32 +8,34 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 <template>
   <SheetContent
     @close-auto-focus="(e) => e.preventDefault()"
-    class="w-1/2 max-w-[540px] p-0"
+    class="w-1/2 max-w-[540px] min-w-[540px] p-0"
   >
     <ScrollArea class="h-screen">
-      <div class="w-full h-48 bg-gray-50 flex items-center justify-center">
-        <Avatar size="lg">
-          <AvatarImage
-            :src="($route.query.image as string) ?? ''"
-            alt="@shadcn"
-          />
-          <AvatarFallback class="text-sm">
-            {{ $route.query.fullname }}
-          </AvatarFallback>
-        </Avatar>
-      </div>
-      <div class="w-full grid grid-cols-2 p-2 gap-2">
-        <div class="flex gap-2 w-full">
-          <User :size="22" />{{ $route.query.fullname }}
+      <div class="w-full h-fit flex px-2">
+        <div class="w-full h-48 flex items-center justify-center border-b">
+          <Avatar size="lg">
+            <AvatarImage
+              :src="($route.query.image as string) ?? ''"
+              alt="@shadcn"
+            />
+            <AvatarFallback class="text-sm">
+              {{ $route.query.fullname }}
+            </AvatarFallback>
+          </Avatar>
         </div>
-        <div class="flex gap-2 w-full">
-          <Mail :size="22" />{{ $route.query.email }}
-        </div>
-        <div class="flex gap-2 w-full">
-          <Phone :size="22" />{{ $route.query.phoneNumber }}
-        </div>
-        <div class="flex gap-2 w-full">
-          <MapPin :size="22" />{{ $route.query.address }}
+        <div class="w-full grid grid-cols-1 p-2 py-6 gap-1 border-b">
+          <div class="flex gap-2 w-full">
+            <User :size="22" />{{ $route.query.fullname }}
+          </div>
+          <div class="flex gap-2 w-full">
+            <Mail :size="22" />{{ $route.query.email }}
+          </div>
+          <div class="flex gap-2 w-full">
+            <Phone :size="22" />{{ $route.query.phoneNumber }}
+          </div>
+          <div class="flex gap-2 w-full">
+            <MapPin :size="22" />{{ $route.query.address }}
+          </div>
         </div>
       </div>
     </ScrollArea>
