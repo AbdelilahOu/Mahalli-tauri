77565c004355aceedd2da845b8f4cf8376be640f
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-07-03 15:26:59 +0000

    FEAT: use i18n for date filter

--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -8,7 +8,7 @@ import { error } from "tauri-plugin-log-api";
 import { toast } from "vue-sonner";
 
 const route = useRoute();
-const { t } = useI18n();
+const { t, d } = useI18n();
 const { toggleModal, setModalName } = useStore();
 const { updateQueryParams } = useUpdateRouteQueryParams();
 
@@ -129,9 +129,7 @@ const updateModal = (name: string) => {
               >
                 <CalendarIcon class="mr-2 h-4 w-4" />
                 <span class="text-nowrap">{{
-                  createdAt
-                    ? new Date(createdAt).toLocaleDateString("fr-fr")
-                    : t("g.pick-date")
+                  createdAt ? d(new Date(createdAt), "short") : t("g.pick-date")
                 }}</span>
               </Button>
             </PopoverTrigger>
