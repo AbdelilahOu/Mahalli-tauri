55fd83697d4dac82272257e9b652e8cf31b012cc
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-08-31 11:11:44 +0000

    chore: update nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,45 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2024-08-31",
   devtools: { enabled: false },
-
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/i18n"],
-
-  sourcemap: false,
-
   ssr: false,
-
-  routeRules: {
-    "/": {
-      prerender: true,
-    },
-  },
-
-  imports: {
-    dirs: ["./schemas"],
-  },
-
-  shadcn: {
-    componentDir: "./components/ui",
-  },
-
+  routeRules: { "/": { prerender: true } },
+  imports: { dirs: ["./schemas"] },
+  shadcn: { componentDir: "./components/ui" },
   i18n: {
     locales: [
-      {
-        code: "en",
-        file: "en-US.json",
-      },
-      {
-        code: "fr",
-        file: "fr-FR.json",
-      },
-      {
-        code: "ar",
-        file: "ar-AE.json",
-      },
-      {
-        code: "de",
-        file: "de-DE.json",
-      },
+      { code: "en", file: "en-US.json" },
+      { code: "fr", file: "fr-FR.json" },
+      { code: "ar", file: "ar-AE.json" },
+      { code: "de", file: "de-DE.json" },
     ],
     langDir: "lang",
     defaultLocale: "en",
