d2d7477371b9883dca43ccc1c683ebe64500ddd6
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-10-07 11:01:51 +0000

    chore: update types

--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,16 @@
 import type { LocationQueryValue } from "vue-router";
 
-declare global {
-  interface Res<T> {
-    error?: string;
-    message?: string;
-    data: T;
-  }
-
-  interface QueryParams {
-    search: LocationQueryValue | LocationQueryValue[];
-    page: number | LocationQueryValue | LocationQueryValue[];
-    status?: LocationQueryValue | LocationQueryValue[];
-    limit: number | LocationQueryValue | LocationQueryValue[];
-    transaction_type?: LocationQueryValue | LocationQueryValue[];
-    created_at?: LocationQueryValue | LocationQueryValue[];
-  }
+export interface Res<T> {
+  error?: string;
+  message?: string;
+  data: T;
 }
+
+export interface QueryParams {
+  search: LocationQueryValue | LocationQueryValue[];
+  page: number | LocationQueryValue | LocationQueryValue[];
+  status?: LocationQueryValue | LocationQueryValue[];
+  limit: number | LocationQueryValue | LocationQueryValue[];
+  transaction_type?: LocationQueryValue | LocationQueryValue[];
+  created_at?: LocationQueryValue | LocationQueryValue[];
+}*
