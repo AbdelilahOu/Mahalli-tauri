4a52778b6af70b097eea43c78ede40bd985f821c
abdelilah ouaadouch <gxgamer710@gmail.com>
2024-10-08 10:31:59 +0000

    chore: update styles

--- components/ui/select/SelectTrigger.vue
+++ components/ui/select/SelectTrigger.vue
@@ -26,8 +26,8 @@ const forwardedProps = useForwardProps(delegatedProps);
     v-bind="forwardedProps"
     :class="
       cn(
-        'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
-        props.class,
+        'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:border-2 focus:border-black disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+        props.class
       )
     "
   >
