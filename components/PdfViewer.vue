95f8d4c634aa34c820741f69bd04636b04256e3b
AbdelilahOu <abdelilah4dev@gmail.com>
2024-10-14 10:37:04 +0000

    chore: clean up

--- components/PdfViewer.vue
+++ components/PdfViewer.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import { ref } from "vue";
-
 defineProps<{
   pdfContent: string;
 }>();
-
-const pdfRef = ref<HTMLIFrameElement | null>(null);
 </script>
 
 <template>
-  <iframe ref="pdfRef" class="flex-1" :src="pdfContent" />
+  <iframe class="flex-1" :src="pdfContent" />
 </template>
