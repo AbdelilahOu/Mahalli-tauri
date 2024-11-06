21d270c8fc2d26e2f6b52d561503afd8bf6fbc18
AbdelilahOu <abdelilah4dev@gmail.com>
2024-11-06 14:38:24 +0000

    feat: new version 1.9.7

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -13,13 +13,13 @@ NEW_VERSION=$1
 # Update package.json in root
 sed -i "s/\"version\": \".*\"/\"version\": \"$NEW_VERSION\"/" "$ROOT_DIR/package.json"
 
-# Update Cargo.toml in src-tauri
-sed -i "s/^version = \".*\"/version = \"$NEW_VERSION\"/" "$ROOT_DIR/src-tauri/Cargo.toml"
+# Update the first occurrence of version in Cargo.toml
+sed -i "1,/^version = / s/version = \".*\"/version = \"$NEW_VERSION\"/" "$ROOT_DIR/src-tauri/Cargo.toml"
 
 # Update RELEASE_VERSION in GitHub workflow
 sed -i "s/RELEASE_VERSION: .*/RELEASE_VERSION: $NEW_VERSION/" "$ROOT_DIR/.github/workflows/releases.yml"
 
 echo "Updated version to $NEW_VERSION in:"
 echo "- package.json"
-echo "- src-tauri/Cargo.toml"
+echo "- src-tauri/Cargo.toml (first occurrence)"
 echo "- .github/workflows/releases.yml"
